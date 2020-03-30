f84e9cf8ff7142daa4603776d7a179b71426853f
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-29 22:44:10 -0400

    Fetch user friends

--- app/storage_service.py
+++ app/storage_service.py
@@ -7,28 +7,20 @@ load_dotenv()
 
 GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS") # implicit check by google.cloud (and keras)
 PROJECT_NAME = os.getenv("BIGQUERY_PROJECT_NAME", default="tweet-collector-py")
-DATASET_NAME = os.getenv("BIGQUERY_DATASET_NAME", default="impeachment_production") #> "impeachment_production"
+DATASET_NAME = os.getenv("BIGQUERY_DATASET_NAME", default="impeachment_development") #> "_test" or "_production"
 
 class BigQueryService():
+
     def __init__(self, project_name=PROJECT_NAME, dataset_name=DATASET_NAME):
         self.project_name = project_name
         self.dataset_name = dataset_name #> "impeachment_production", "impeachment_test", etc.
-
         self.client = bigquery.Client()
-
         self.dataset_ref = self.client.dataset(self.dataset_name)
         self.dataset_address = f"{self.project_name}.{self.dataset_name}"
-        print("BIGQUERY DATASET:", self.dataset_address.upper())
-
-        self.tweets_table_name = "tweets"
-        self.tweets_table_ref = self.dataset_ref.table(self.tweets_table_name)
-        self.tweets_table = self.client.get_table(self.tweets_table_ref) # an API call (caches results for subsequent inserts)
-        self.tweets_table_address = f"{self.dataset_address}.{self.tweets_table_name}"
-
-        self.topics_table_name = "topics"
-        self.topics_table_ref = self.dataset_ref.table(self.topics_table_name)
-        self.topics_table = self.client.get_table(self.topics_table_ref) # an API call (caches results for subsequent inserts)
-        self.topics_table_address = f"{self.dataset_address}.{self.topics_table_name}"
+        #self.tweets_table_ref = self.dataset_ref.table("tweets")
+        #self.tweets_table = self.client.get_table(self.tweets_table_ref) # an API call (caches results for subsequent inserts)
+        #self.topics_table_ref = self.dataset_ref.table("topics")
+        #self.topics_table = self.client.get_table(self.topics_table_ref) # an API call (caches results for subsequent inserts)
 
     def execute_query(self, sql):
         """Param: sql (str)"""
@@ -36,36 +28,69 @@ class BigQueryService():
         return job.result()
 
     def fetch_topics(self):
-        """Returns a list of table rows"""
         sql = f"""
             SELECT topic, created_at
-            FROM `{self.topics_table_address}`
+            FROM `{self.dataset_address}.topics`
             ORDER BY created_at;
         """
         results = self.execute_query(sql)
         return list(results)
 
+    def migrate_user_friends(self):
+        sql = f"""
+            CREATE TABLE IF NOT EXISTS {self.dataset_address}.user_friends as (
+                SELECT
+                    distinct(status_id) as user_id
+                    ,NULL as friend_ids
+                FROM `{self.dataset_address}.tweets`
+                ORDER BY 1
+            );
+        """
+        results = self.execute_query(sql)
+        return list(results)
 
-
+    def fetch_user_friends(self):
+        """Returns a list of table rows"""
+        sql = f"""
+            SELECT user_id, friend_ids
+            FROM `{self.dataset_address}.user_friends`
+            ORDER BY user_id;
+        """
+        results = self.execute_query(sql)
+        print(type(results))
+        print(results)
+        return list(results)
 
 if __name__ == "__main__":
 
-        bq_service = BigQueryService()
+        service = BigQueryService()
+        print("BIGQUERY DATASET:", service.dataset_address.upper())
+
+        user_friends = service.fetch_user_friends()
+        for row in user_friends:
+            print("USER:", row.user_id)
+
+
+        exit()
+
+
+
+
+
+
 
         print("--------------------")
         print("FETCHING TOPICS...")
-        results = bq_service.fetch_topics()
+        results = service.fetch_topics()
         for row in results:
             print(row)
             print("---")
 
         print("--------------------")
         print("COUNTING TWEETS...")
-        sql = f"SELECT count(distinct status_id) as tweets_count FROM `{bq_service.tweets_table_address}`"
-        results = bq_service.execute_query(sql)
+        sql = f"SELECT count(distinct status_id) as tweets_count FROM `{service.dataset_address}.tweets`"
+        results = service.execute_query(sql)
         print(list(results)[0].tweets_count)
-
-        exit()
 
         print("--------------------")
         print("FETCHING LATEST TWEETS...")
@@ -73,11 +98,19 @@ if __name__ == "__main__":
             SELECT
                 status_id, status_text, geo, created_at,
                 user_id, user_screen_name, user_description, user_location, user_verified
-            FROM `{bq_service.tweets_table_address}`
+            FROM `{service.dataset_address}.tweets`
             ORDER BY created_at DESC
             LIMIT 3
         """
-        results = bq_service.execute_query(sql)
+        results = service.execute_query(sql)
         for row in results:
             print(row)
             print("---")
+
+
+
+
+        exit()
+
+        #results = service.migrate_user_friends()
+        #print(results)
