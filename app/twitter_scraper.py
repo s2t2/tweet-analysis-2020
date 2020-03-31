465086e2e4ce064da669effb4ba057f7dfa87699
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-31 16:33:26 -0400

    Adapt previous scraper which returns screen names as well

--- app/twitter_scraper.py
+++ app/twitter_scraper.py
@@ -12,12 +12,11 @@ load_dotenv()
 SCREEN_NAME = os.getenv("TWITTER_SCREEN_NAME", default="elonmusk") # just one to use for testing purposes
 
 def get_friends(screen_name, max_friends=2000):
+    print("USER:", screen_name)
 
     request_url = f"https://mobile.twitter.com/{screen_name}/following"
-
     cookie_jar = CookieJar()
-    print(type(cookie_jar))
-
+    #print(type(cookie_jar)) #> <class 'http.cookiejar.CookieJar'>
     opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie_jar))
     headers = [
         ('Host', "twitter.com"),
@@ -29,46 +28,47 @@ def get_friends(screen_name, max_friends=2000):
         ('Connection', "keep-alive")
     ]
     opener.addheaders = headers
-    print(type(opener))
+    #print(type(opener)) #> <class 'urllib.request.OpenerDirector'>
 
-    try:
-        response = opener.open(request_url)
-        print(type(response))
+    response = opener.open(request_url)
+    #print(type(response)) #> <class 'http.client.HTTPResponse'>
 
-        response_body = response.read()
-        print(type(response_body))
+    response_body = response.read()
+    #print(type(response_body)) #> bytes
 
-        soup = BeautifulSoup(response_body.decode(), "html.parser")
-        print(type(soup))
-        print(soup.prettify())
+    soup = BeautifulSoup(response_body.decode(), "html.parser")
+    #print(type(soup)) #> <class 'bs4.BeautifulSoup'>
+    #print(soup.prettify())
 
-        breakpoint()
+    #
+    # <span class="count">262</span>
+    #
+    friends_count = int(soup.find("span", "count").text)
+    print("FRIENDS COUNT:", friends_count)
 
+    #
+    # <form action="/i/guest/follow/SCREEN_NAME_X" method="post">
+    #   <span class="m2-auth-token">
+    #     <input name="authenticity_token" type="hidden" value="..."/>
+    #   </span>
+    #   <span class="w-button-common w-button-follow">
+    #     <input alt="..." src="https://ma.twimg.com/twitter-mobile/.../images/sprites/followplus.gif" type="image"/>
+    #   </span>
+    # </form>
+    #
+    forms = soup.find_all("form")
+    substr = "/i/guest/follow/"
+    friend_names = [f.attrs["action"].replace(substr, "") for f in forms if substr in f.attrs["action"]]
+    print("FRIENDS PAGE:", friend_names) #> 20
 
-        parsed_response = response_body.decode().split("\n")
-        print(type(parsed_response))
-
-
-        #
-        #### Parse JSON code to uncover latest number of friends of that user
-        #try :
-        #    nfriends=[int(''.join(i.split('"count">')[1].split('</span>')[0].split('.'))) for i in res if '<td class="info"><span class="count">' in i ][0]
-        #except Exception as e: ##protected or suspended user
-        #    print('Error occcured: ', e)
-        #    accessible=False
-        #    INACCESSIBLE.append(toquery[count-1])
-        #
-
-        #if(accessible):
-        #    #if count%10==0:
-        #    print('\tUser %s of %s: %s has %s following, we will only get %s'%(count,n,screen_name,nfriends,max_friends))
-        #
-        #    ufriends+=[i.split('/follow/')[1].split('"')[0] for i in res if '/i/guest/follow/' in i ]
-        #    cnext=[i.split('cursor=')[1].split('"')[0] for i in res if screen_name+'/following?cursor=' in i]
-        #        #print('\t%s: already got %s friends '%(user,len(ufriends)))
-
-    except Exception as err:
-        print("OOPS", err)
+    #
+    # <div class="w-button-more">
+    #   <a href="/SCREEN_NAME/following?cursor=CURSOR_ID">...</a>
+    # </div>
+    #
+    next_link = soup.find("div", "w-button-more").find("a")
+    next_cursor_id = next_link.attrs["href"].split("/following?cursor=")[-1]
+    print("NEXT CURSOR ID:", next_cursor_id)
 
     #### Keep iterating through pages as long as we find results
     #if (len(cnext)>0 and keepgoing and accessible):
@@ -124,8 +124,10 @@ def get_friends(screen_name, max_friends=2000):
     #
     #print('Finished crawling following network.  Took ', time.time()-start)
 
+    return friend_names
+
 if __name__ == "__main__":
 
-    print("USER:", SCREEN_NAME)
+    #print("USER:", SCREEN_NAME)
     friend_ids = get_friends(SCREEN_NAME)
-    print("FRIENDS COUNT:", len(friend_ids))
+    #print("FRIENDS COUNT:", len(friend_ids))
