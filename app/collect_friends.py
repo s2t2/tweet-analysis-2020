e74fb0d1d84062177ffebb15230ff631cb7c0993
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-30 21:56:02 -0400

    Go back to using a batch counter instead of multiple len() calls

--- app/collect_friends.py
+++ app/collect_friends.py
@@ -2,14 +2,11 @@
 from app.storage_service import BigQueryService
 from app.twitter_service import twitter_faster_api, get_friends
 
-UNIVERSE_SIZE = 450
+MAX_USERS = 450
 BATCH_SIZE = 200
 MAX_FRIENDS = 2000
 
 if __name__ == "__main__":
-
-    api = twitter_faster_api()
-    print("TWITTER API CLIENT", api)
 
     service = BigQueryService()
     print("BIGQUERY DATASET:", service.dataset_address.upper())
@@ -20,11 +17,15 @@ if __name__ == "__main__":
 
     service.init_tables()
 
-    users = service.fetch_remaining_users(limit=UNIVERSE_SIZE)
+    users = service.fetch_remaining_users(limit=MAX_USERS)
     users_count = len(users)
     print("FETCHED", users_count, "USERS...")
 
+    api = twitter_faster_api()
+    print("TWITTER API CLIENT", api)
+
     batch = []
+    batch_size = 0
     for row_index, row in enumerate(users):
         print("INDEX:", row_index) # starts at zero
         print("USER:", row.user_id)
@@ -36,10 +37,12 @@ if __name__ == "__main__":
             "friends_count": len(friend_ids),
             "friend_ids": friend_ids,
         })
+        batch_size += 1
 
         # store full batches, and the final batch, even if the final batch isn't full
-        if (len(batch) >= BATCH_SIZE) or (row_index + 1 >= users_count):
-            print(f"SAVING AND CLEARING THE BATCH (SIZE: {batch_size})...")
+        if (batch_size >= BATCH_SIZE) or (row_index + 1 >= users_count):
+            print(f"SAVING A BATCH OF USERS (SIZE: {batch_size})...")
             service.append_user_friends(batch)
             batch = []
             batch_size = 0
+            print(f"CLEARING THE BATCH (SIZE: {batch_size})...")
