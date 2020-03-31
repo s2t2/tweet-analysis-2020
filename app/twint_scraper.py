22677a304ff8ecefb3c61a9a951ee76f3fe540e7
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-31 17:12:12 -0400

    Refactor using OOP

--- app/twint_scraper.py
+++ app/twint_scraper.py
@@ -10,83 +10,54 @@ load_dotenv()
 
 SCREEN_NAME = os.getenv("TWITTER_SCREEN_NAME", default="elonmusk") # just one to use for testing purposes
 
-def init_config(screen_name, max_friends, verbose=True):
-    config = twint.Config()
-    config.Username = screen_name
-    config.Limit = max_friends
-    config.Hide_output = False
-    config.Store_object = True
-    return config
-
-def get_friend_names(screen_name=None, max_friends=2000):
-    """ (FASTER APPROACH)
-    Params:
-        screen_name like "barackobama" or "s2t2"
-        max_friends (int)
-    Returns a list of the user's friend screen names (or empty list if the account was private)
+class TwitterScraper():
+    """ See:
+        https://pielco11.ovh/posts/twint-osint/#followersfollowing
+        https://github.com/twintproject/twint/wiki/Storing-objects-in-RAM
+        https://github.com/twintproject/twint/pull/685
+        https://github.com/twintproject/twint/issues/704
+        https://github.com/twintproject/twint/issues/270
     """
-    config = init_config(screen_name, max_friends)
-    config.User_full = False # a faster approach, but only has screen names
-    config.Store_object_follow_list = [] # initialize a place to store the screen names
-    twint.run.Following(config)
-    return config.Store_object_follow_list
 
-def get_friend_ids(screen_name=None, max_friends=2000):
-    """ (SLOWER APPROACH)
-    Params:
-        screen_name like "barackobama" or "s2t2" or
-        max_friends (int)
-    Returns a list of the user's friend_ids (or empty list if the account was private)
-    """
-    config = init_config(screen_name, max_friends)
-    config.User_full = True # a somewhat slow approach, but has ids
-    twint.run.Following(config)
-    #print("USERS LIST:", twint.output.users_list) #> []
-    return [obj["id"] for obj in twint.output.users_list]
+    def __init__(self, screen_name, max_friends=2000, verbose=False):
+        """ Params:
+            screen_name (str) like "barackobama" or "s2t2"
+            max_friends (int)
+            verbose (bool)
+        """
+        self.screen_name = screen_name
+        self.max_friends = max_friends
+        self.verbose = verbose
+
+        self.basic_config = twint.Config()
+        self.basic_config.Username = screen_name
+        self.basic_config.Limit = max_friends
+        self.basic_config.Hide_output = (verbose == False)
+        self.basic_config.Store_object = True
+
+    def get_friend_names(self):
+        """ (FASTER APPROACH)
+        Returns a list of the user's friends' screen names (or empty list if the account was private)
+        """
+        config = self.basic_config
+        config.User_full = False # a faster approach, but only has screen names
+        config.Store_object_follow_list = [] # initialize a place to store the screen names
+        twint.run.Following(config)
+        return config.Store_object_follow_list
+
+    def get_friend_ids(self):
+        """ (SLOWER APPROACH)
+        Returns a list of the user's friends' ids (or empty list if the account was private)
+        """
+        config = self.basic_config
+        config.User_full = True # a somewhat slow approach, but has ids
+        twint.run.Following(config)
+        return [obj["id"] for obj in twint.output.users_list]
 
 if __name__ == "__main__":
 
-    print("USER:", SCREEN_NAME)
-    #friend_ids = get_friend_ids(SCREEN_NAME)
-    #print("FRIENDS COUNT:", len(friend_ids))
+    scraper = TwitterScraper(screen_name=SCREEN_NAME)
+    print("USER:", scraper.screen_name)
 
-    friend_names = get_friend_names(SCREEN_NAME)
+    friend_names = scraper.get_friend_names()
     print("FRIENDS COUNT:", len(friend_names))
-
-
-
-    exit()
-
-    # h/t:
-    #   https://pielco11.ovh/posts/twint-osint/#followersfollowing
-    #   https://github.com/twintproject/twint/wiki/Storing-objects-in-RAM
-    #   https://github.com/twintproject/twint/pull/685
-    #   https://github.com/twintproject/twint/issues/704
-    #   https://github.com/twintproject/twint/issues/270
-
-    config = twint.Config()
-    config.Username = SCREEN_NAME
-    config.Limit = 2000
-    config.Hide_output = True
-    config.Store_object = True
-    config.User_full = True
-    #config.Pandas = True
-    config.Store_object_follow_list = []
-    #config.Custom = ['id']
-    #config.Custom["user"] = ['id']
-    #config.Format = "ID {id} | Username {username}"
-    print(config)
-
-    twint.run.Following(config)
-    # takes a few requests
-    # seeing the occasional "CRITICAL:root:twint.feed:Follow:IndexError"
-
-    print("FOLLOWS LIST:", twint.output.follows_list) #> []
-    print("USERS LIST:", twint.output.users_list) #> []
-    print("STORED OBJECT FOLLOWS LIST:", config.Store_object_follow_list)
-
-    #print(dir(twint.storage.panda))
-    #print(twint.storage.panda.Follow_df.head())
-
-    friend_ids = [obj["id"] for obj in twint.output.users_list]
-    print(friend_ids)
