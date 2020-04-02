222db92d752e236830c469ae0f2ce0972ed55ff3
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-02 19:23:33 -0400

    Compiling results as they come in

--- app/executor.py
+++ app/executor.py
@@ -10,24 +10,21 @@ from threading import Thread, current_thread
 
 load_dotenv()
 
-MAX_THREADS = int(os.getenv("MAX_THREADS", 5)) # heroku supports max 256, see: https://devcenter.heroku.com/articles/dynos#process-thread-limits
+MAX_THREADS = int(os.getenv("MAX_THREADS", 3)) # heroku supports max 256, see: https://devcenter.heroku.com/articles/dynos#process-thread-limits
 
 def fetch_friends(user_id, sleep_seconds=1):
-    thread_id = current_thread().name
-
-    #print(user_id, "SLEEPING...")
+    thread_id = int(current_thread().name.replace("THREAD_", "")) + 1
     time.sleep(sleep_seconds)
-    #print(user_id, "DONE")
-    return {"user_id": user_id, "thread_id": thread_id, "duration": sleep_seconds}
+    return {"thread_id": thread_id, "user_id": user_id, "duration": sleep_seconds}
 
 if __name__ == "__main__":
 
-    user_ids = range(0,25)
+    user_ids = range(1,11)
     start_at = time.perf_counter()
     print(f"USERS: {len(user_ids)}")
     print(f"THREADS: {MAX_THREADS}")
 
-    with ThreadPoolExecutor(max_workers=MAX_THREADS, thread_name_prefix="THR") as executor:
+    with ThreadPoolExecutor(max_workers=MAX_THREADS, thread_name_prefix="THREAD") as executor:
         #print("EXECUTOR:", type(executor))
 
         #results = executor.map(fetch_friends, user_ids, random.choice([1,5]))
@@ -38,9 +35,15 @@ if __name__ == "__main__":
         #for future in futures:
         #    print(future.result())
 
+        results = []
         futures = [executor.submit(fetch_friends, user_id, random.choice([1,5])) for user_id in user_ids]
         for future in as_completed(futures):
-            print(future.result())
+            result = future.result()
+            print(result)
+            results.append(result)
 
     end_at = time.perf_counter()
-    print(f"PROCESSED {len(user_ids)} IN {round(end_at - start_at, 2)} SECONDS")
+    clock_seconds = round(end_at - start_at, 2)
+    total_seconds = sum([result["duration"] for result in results])
+
+    print(f"PROCESSED {len(user_ids)} USERS IN {clock_seconds} SECONDS (OTHERWISE {total_seconds} SECONDS)")
