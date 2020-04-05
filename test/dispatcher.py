2b8ad84a73c5251c0e07ccb00bc43818b09055d4
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-05 12:13:55 -0400

    This seems to be working ok

--- test/dispatcher.py
+++ test/dispatcher.py
@@ -10,23 +10,32 @@ from random import choice
 from threading import current_thread, BoundedSemaphore
 from concurrent.futures import ThreadPoolExecutor, as_completed # see: https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
 
+TOTAL_USERS = 300
+BATCH_SIZE = 100
+MAX_THREADS = 10
+
 def process_batch(user_ids):
     print(f"PROCESSING {len(user_ids)} USERS (FROM {min(user_ids)} TO {max(user_ids)})")
+    #results = []
+    #for user_id in user_ids:
+    #    result = fetch_friends(user_id, choice([1,3]))
+    #    print(result)
+    #    results.append(result)
+    #return results
+
     results = []
-    for user_id in user_ids:
-        result = fetch_friends(user_id, choice([1,3]))
-        print(result)
-        results.append(result)
+    with ThreadPoolExecutor(max_workers=MAX_THREADS, thread_name_prefix="THREAD") as executor:
+        for result in executor.map(fetch_friends, user_ids):
+            print(result)
+            results.append(result)
     return results
 
-def fetch_friends(user_id, sleep_seconds=10):
+def fetch_friends(user_id, sleep_seconds=None):
+    sleep_seconds = sleep_seconds or choice([1,3])
     time.sleep(sleep_seconds)
     return {"user_id": user_id, "duration": sleep_seconds, "thread_id": current_thread().name.split("_")[-1]}
 
 if __name__ == "__main__":
-
-    TOTAL_USERS = 300
-    BATCH_SIZE = 100
 
     print("----------------")
     start_at = time.perf_counter()
