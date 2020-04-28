77986435a5d794fc06adbd2664885d7e72d86bc3
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-28 17:56:04 -0400

    OK this is going to work

--- test/start/test_network_grapher.py
+++ test/start/test_network_grapher.py
@@ -1,15 +1,12 @@
 import os
-import networkx as nx
 import pandas
+from networkx import DiGraph, Graph
 
-from app.storage_service import BigQueryService
-
-GPICKLE_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "..", "data", "network.gpickle")
-
-CSV_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "..", "data", "example_network.csv")
 # columns: screen_name, friend_1, friend_2, friend_3, friend_4, etc...
+#CSV_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "..", "data", "example_network.csv")
+MOCK_CSV_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "data", "mock_network.csv")
 
-def compile_nodes_and_edges(screen_names, csv_filepath=CSV_FILEPATH):
+def compile_nodes_and_edges(screen_names, csv_filepath=MOCK_CSV_FILEPATH):
     """
     Given the following network:
 
@@ -55,7 +52,7 @@ def generate_graph(edges):
 
     Returns graph (networkx.classes.digraph.DiGraph)
     """
-    graph = nx.DiGraph()
+    graph = DiGraph()
     for edge in edges:
         source = edge[0] # source, a.k.a friend, a.k.a followed
         recipient = edge[1] # recipient, a.k.a user, a.k.a follower
@@ -64,28 +61,37 @@ def generate_graph(edges):
         graph.add_edge(source, recipient)
     return graph
 
-if __name__ == "__main__":
+def test_nodes_and_edges():
+    #
+    # Given:
+    #     A doesn't follow anyone
+    #     B follows A
+    #     C follows B and A
+    #     D and E follow eachother
+    #
+    df = pandas.read_csv(MOCK_CSV_FILEPATH, header=None)
+    screen_names = df[0].tolist()
 
-    #df = pandas.read_csv(CSV_FILEPATH, header=None)
-    #screen_names = df[0].tolist()
-    #nodes, edges = compile_nodes_and_edges(screen_names)
-    #print("NODE COUNT:", len(nodes))
-    #print("EDGE COUNT:", len(edges))
+    nodes, edges = compile_nodes_and_edges(screen_names, csv_filepath=MOCK_CSV_FILEPATH)
+    assert nodes == {'A': 1, 'B': 1, 'C': 1, 'D': 1, 'E': 1}
+    assert edges == [('A', 'B'), ('B', 'C'), ('A', 'C'), ('E', 'D'), ('D', 'E')]
 
-    graph = generate_graph(edges)
-    print(type(graph)) #> <class 'networkx.classes.digraph.DiGraph'>
+def test_graph_generation():
+    graph = generate_graph([('A', 'B'), ('B', 'C'), ('A', 'C'), ('E', 'D'), ('D', 'E')])
+    assert isinstance(graph, DiGraph)
+    assert len(graph.edges) == 5
+    assert len(graph.nodes) == 5
 
-    service = BigQueryService.cautiously_initialized()
-    user_friends = service.fetch_user_friends(limit=20)
+def test_undirected():
+    # https://networkx.github.io/documentation/latest/reference/classes/generated/networkx.DiGraph.to_undirected.html#networkx.DiGraph.to_undirected
+    graph = generate_graph([('A', 'B'), ('B', 'A')])
+    undirected = graph.to_undirected()
+    assert isinstance(graph, DiGraph) and isinstance(undirected, Graph)
+    assert len(graph.nodes) == 2 and len(undirected.nodes) == 2
+    assert len(graph.edges) == 2 and len(undirected.edges) == 1
 
-    breakpoint()
-    graph = nx.DiGraph()
-    for edge in edges:
-        source = edge[0] # source, a.k.a friend, a.k.a followed
-        recipient = edge[1] # recipient, a.k.a user, a.k.a follower
-        graph.add_node(source)
-        graph.add_node(recipient)
-        graph.add_edge(source, recipient)
-
-    print("WRITING NETWORK GRAPH TO:", GPICKLE_FILEPATH)
-    nx.write_gpickle(graph, GPICKLE_FILEPATH)
+def test_duplication():
+    graph = DiGraph()
+    graph.add_node("A")
+    graph.add_node("A")
+    assert len(graph.nodes) == 1
