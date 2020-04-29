8d4d1367185c46294b363422166321c0e8ca875b
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-28 20:42:11 -0400

    Maybe built-in pagination just works

--- app/workers/network_grapher.py
+++ app/workers/network_grapher.py
@@ -1,25 +1,77 @@
 
+import time
 import os
 from networkx import DiGraph, write_gpickle
+from google.cloud import bigquery
 
 from app.storage_service import BigQueryService
 
 GPICKLE_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "..", "data", "follower_network.gpickle")
+
+def process_batch(graph, batch):
+    for row in batch:
+        user = row["screen_name"]
+        graph.add_node(user)
+
+        for friend in row["friend_names"]:
+            graph.add_node(friend)
+            graph.add_edge(user, friend)
+
+    return graph
 
 if __name__ == "__main__":
 
     graph = DiGraph()
 
     service = BigQueryService.cautiously_initialized()
+    #user_friends = service.fetch_user_friends(limit=100) # TODO: fetch in batches
+    #process_batch(graph, user_friends)
 
-    user_friends = service.fetch_user_friends(limit=100) # TODO: fetch in batches
-    for row in user_friends:
-        user = row.screen_name
-        graph.add_node(user)
+    # how to fetch in batches?
+    # https://cloud.google.com/bigquery/docs/running-queries#batch
+    # https://cloud.google.com/bigquery/docs/paging-results
 
-        for friend in row.friend_names:
-            graph.add_node(friend)
-            graph.add_edge(user, friend)
+    start_at = time.perf_counter()
+    counter = 1
+
+    #rows = service.client.list_rows(f"{service.dataset_address}.user_friends") #, max_results=10
+    #for row in rows:
+    #    #print(row["screen_name"], row["friend_count"])
+    #    counter+=1
+
+    sql = f"""
+        SELECT user_id, screen_name, friend_count, friend_names
+        FROM `{service.dataset_address}.user_friends`
+        ORDER BY screen_name;
+    """
+    client = service.client
+    job_config = bigquery.QueryJobConfig(priority=bigquery.QueryPriority.BATCH)
+    job = client.query(sql, job_config=job_config)
+    #job = client.get_job(job.job_id, location=job.location)  # Make an API request.
+    print(job.job_id, job.state)
+
+    for row in job:
+        counter+=1
+
+
+
+
+
+
+
+    end_at = time.perf_counter()
+    duration_seconds = round(end_at - start_at, 2)
+    print(f"PROCESSED {counter} USERS IN {duration_seconds} SECONDS")
+
+    exit()
+
+
+
+
+
+
+
+
 
     print("NETWORK GRAPH COMPLETE!")
     print("NODES:", len(graph.nodes))
