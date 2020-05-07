4a7cf1c22f7d4f378968dcba36564582ac427430
Mike Rossetti <s2t2@users.noreply.github.com>
2020-05-07 19:04:27 -0400

    Migrate user friends

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, Column, Integer, String, ARRAY
+from sqlalchemy import create_engine, Column, Integer, BigInteger, String, ARRAY, TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
@@ -22,15 +22,22 @@ class Book(Base):
     author = Column(String(128))
     readers = Column(ARRAY(String(128)))
 
-if __name__ == "__main__":
+class UserFriend(Base):
+    __tablename__ = "user_friends"
+    user_id = Column(BigInteger, primary_key=True)
+    screen_name = Column(String(128))
+    friend_count = Column(Integer)
+    friend_names = Column(ARRAY(String(128)))
+    start_at = Column(TIMESTAMP)
+    end_at = Column(TIMESTAMP)
 
-    #base.metadata.create_all(db)
+if __name__ == "__main__":
 
     #Book.__table__.drop(db)
     #Book.__table__.create(db)
-
-    if not Book.__table__.exists():
-        Book.__table__.create(db)
+    #if not Book.__table__.exists(): Book.__table__.create(db)
+    #if not UserFriend.__table__.exists(): UserFriend.__table__.create(db)
+    Base.metadata.create_all(db)
 
     session = BoundSession()
 
@@ -46,5 +53,13 @@ if __name__ == "__main__":
     books = session.query(Book)
     for book in books:
         print("...", book.id, "|", book.title, "|", book.author, "|", book.readers)
+
+
+    print("-------")
+    print("USER FRIENDS:")
+    results = db.execute("SELECT count(DISTINCT screen_name) as row_count FROM user_friends;")
+    #print(results.keys()) #> ["row_count"]
+    #print(results.rowcount) #> 1
+    print("...", results.fetchone()[0]) #TODO: row factory ... results.fetchone()["row_count"]
 
     session.close()
