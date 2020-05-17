40b92da08a9a6131a56434c591ff2a10a39f79b6
Mike Rossetti <s2t2@users.noreply.github.com>
2020-05-17 16:51:45 -0400

    Generating small graphs

--- app/workers/psycopg_grapher.py
+++ app/workers/psycopg_grapher.py
@@ -1,5 +1,7 @@
 
 import time
+import os
+from datetime import datetime as dt
 import psycopg2
 from networkx import DiGraph, write_gpickle
 
@@ -32,31 +34,46 @@ class NetworkGrapher():
                 exit()
         return service
 
+    #@profile
     def perform(self):
         self.counter = 0
         self.start_at = time.perf_counter()
 
-        if not self.dry_run:
-            print("GENERATING NETWORK GRAPH...")
-
-        sql = f"SELECT id, user_id, screen_name, friend_count, friend_names FROM {USER_FRIENDS_TABLE_NAME}"
+        sql = f"""
+            SELECT id, user_id, screen_name, friend_count, friend_names
+            FROM {USER_FRIENDS_TABLE_NAME}
+        """
         self.cursor.execute(sql)
 
-        while True:
-            results = self.cursor.fetchmany(size=BATCH_SIZE)
-            if not results: break
+        #while True:
+        #    results = self.cursor.fetchmany(size=BATCH_SIZE)
+        #    if not results: break
+        #    if not self.dry_run:
+        #        for row in results:
+        #            user = row["screen_name"]
+        #            friends = row["friend_names"]
+        #            self.graph.add_node(user)
+        #            self.graph.add_nodes_from(friends)
+        #            self.graph.add_edges_from([(user, friend) for friend in friends])
+#
+        #    self.counter += len(results)
+        #    print(generate_timestamp(), self.counter)
 
-            #for row in results:
-            #    user = row.screen_name
-            #    friends = row.friend_names
-            #    if not self.dry_run:
-            #        self.graph.add_node(user)
-            #        self.graph.add_nodes_from(friends)
-            #        self.graph.add_edges_from([(user, friend) for friend in friends])
+        results = self.cursor.fetchmany(size=BATCH_SIZE)
+        while results:
+            if not self.dry_run:
+                for row in results:
+                    user = row["screen_name"]
+                    friends = row["friend_names"]
+                    self.graph.add_node(user)
+                    self.graph.add_nodes_from(friends)
+                    self.graph.add_edges_from([(user, friend) for friend in friends])
 
             self.counter += len(results)
             print(generate_timestamp(), self.counter)
+            results = self.cursor.fetchmany(size=BATCH_SIZE)
 
+        print("-----------------")
         print("JOB COMPLETE!")
         self.end_at = time.perf_counter()
         self.cursor.close()
@@ -70,7 +87,11 @@ class NetworkGrapher():
             print("EDGES:", len(self.graph.edges))
             print("SIZE:", self.graph.size())
 
-    def write_to_file(self, graph_filepath):
+    def write_to_file(self, graph_filepath=None):
+        if not graph_filepath:
+            timestamp = dt.now().strftime("%Y_%m_%d_%H_%M")
+            graph_filepath = os.path.join(DATA_DIR, f"follower_network_{timestamp}.gpickle")
+
         print("WRITING NETWORK GRAPH TO:", os.path.abspath(graph_filepath))
         write_gpickle(self.graph, graph_filepath)
 
@@ -82,6 +103,7 @@ if __name__ == "__main__":
     grapher = NetworkGrapher()
     grapher.perform()
     grapher.report()
+    grapher.write_to_file()
 
     exit()
 
