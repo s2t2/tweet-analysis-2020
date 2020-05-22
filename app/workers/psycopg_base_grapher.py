e9f4b0044a83831932e1fad273dbad09bd7b0d07
Mike Rossetti <s2t2@users.noreply.github.com>
2020-05-22 14:12:08 -0400

    Rolling our own edges set

--- app/workers/psycopg_base_grapher.py
+++ app/workers/psycopg_base_grapher.py
@@ -11,8 +11,7 @@ from app.workers import BATCH_SIZE, DRY_RUN, generate_timestamp
 
 class BaseGrapher():
     def __init__(self, database_url=DATABASE_URL, table_name=USER_FRIENDS_TABLE_NAME, batch_size=BATCH_SIZE, dry_run=DRY_RUN):
-        self.connection = psycopg2.connect(database_url)
-        self.cursor = self.connection.cursor(name="network_grapher", cursor_factory=psycopg2.extras.DictCursor) # A NAMED CURSOR PREVENTS MEMORY ISSUES!!!!
+        self.database_url = database_url
         self.table_name = table_name
         self.batch_size = batch_size
         self.dry_run = (dry_run == True)
@@ -37,7 +36,6 @@ class BaseGrapher():
     def sql(self):
         return f"SELECT id, user_id, screen_name, friend_count, friend_names FROM {self.table_name};"
 
-    #@profile
     def perform(self):
         """ TODO: have any child perform method automatically wrapped with start() and end() invocations
         Override this method in the child class. Make sure to:
@@ -52,8 +50,10 @@ class BaseGrapher():
     def start(self):
         print("-----------------")
         print("JOB STARTING!")
-        self.counter = 0
+        self.connection = psycopg2.connect(self.database_url)
+        self.cursor = self.connection.cursor(name="network_grapher", cursor_factory=psycopg2.extras.DictCursor) # A NAMED CURSOR PREVENTS MEMORY ISSUES!!!!
         self.start_at = time.perf_counter()
+        self.counter = 0
 
     def end(self):
         print("-----------------")
@@ -63,6 +63,9 @@ class BaseGrapher():
         self.end_at = time.perf_counter()
         self.duration_seconds = round(self.end_at - self.start_at, 2)
         print(f"PROCESSED {self.counter} USERS IN {self.duration_seconds} SECONDS")
+        self.report()
+
+    def report(self):
         print("NODES:", len(self.graph.nodes))
         print("EDGES:", len(self.graph.edges))
         print("SIZE:", self.graph.size())
--- app/workers/psycopg_grapher.py
+++ app/workers/psycopg_grapher.py
@@ -9,23 +9,28 @@ class Grapher(BaseGrapher):
     @profile
     def perform(self):
         self.nodes = set() # using a set to prevent duplicate values from being added
+        self.edges = set()
         self.cursor.execute(self.sql)
         while True:
             results = self.cursor.fetchmany(size=self.batch_size)
             if not results: break
-            self.counter += len(results)
-            print(self.generate_timestamp(), self.counter, len(self.nodes))
-
             if not self.dry_run:
                 for row in results:
-                    self.nodes.add(row["screen_name"])
-                    #for friend_name in row["friend_names"]:
-                    #    self.nodes.add(friend_name)
-                    self.nodes.update(row["friend_names"])
+                    user = row["screen_name"]
+                    self.nodes.add(user)
+                    friends = row["friend_names"]
+                    self.nodes.update(friends)
+                    self.edges.update([(user, friend) for friend in friends])
+            self.counter += len(results)
+            print(self.generate_timestamp(), self.counter, len(self.nodes), len(self.edges))
 
-        print("NODES:", len(self.nodes))
         #self.nodes = sorted(self.nodes) # takes a long time!
-        self.graph = DiGraph() # todo: construct from self.nodes
+        #self.graph = DiGraph() # todo: construct from self.nodes
+
+    def report(self):
+        """Override parent until we get a graph object"""
+        print("NODES:", len(self.nodes))
+        print("EDGES:", len(self.edges))
 
 if __name__ == "__main__":
 
@@ -33,4 +38,4 @@ if __name__ == "__main__":
     grapher.start()
     grapher.perform()
     grapher.end()
-    grapher.write_to_file()
+    #grapher.write_to_file()
