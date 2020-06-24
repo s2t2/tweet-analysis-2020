bc4a8e4ac96a3aa5ece0170ffec6a57434bb74e8
Mike Rossetti <s2t2@users.noreply.github.com>
2020-06-24 16:58:13 -0400

    Making my way through the botcode

--- app/workers/investigate_mpi.py
+++ app/workers/investigate_mpi.py
@@ -1,10 +1,36 @@
 
 
 
+import os
 
 from mpi4py import MPI
+import numpy as np
 
-class ClusterManager(object):
+# HYPERPARAMETERS
+
+MU = float(os.getenv("MU", default="1")) # called "gamma" in the paper
+ALPHA_1 = float(os.getenv("ALPHA_1", default="100"))
+ALPHA_2 = float(os.getenv("ALPHA_2", default="100"))
+ALPHA = [MU, ALPHA_1, ALPHA_2]
+
+N_ITERS = int(os.getenv("N_ITERS", default="1"))
+DIRNAME = os.getenv("DIRNAME", default="impeachment-dev")
+PRIORS_MODE = os.getenv("PRIORS_MODE", default="normal") # should be one of ["boto", "random_unif", "random_gaus"]
+
+LAMBDA_1 = float(os.getenv("LAMBDA_1", default="0.8")) # called "lamba11" in the paper
+LAMBDA_2 = float(os.getenv("LAMBDA_2", default="0.6")) # called "lambda00" in the paper
+EPSILON = float(os.getenv("EPSILON", default="0.001")) # called "delta" in the paper. should be close to 0 (eg. 0.001) in order for lambda10 to be slightly > (lambda00 + lambda11 - 1).
+
+RANDOM_SEED = int(os.getenv("RANDOM_SEED", default="0")) # called "delta" in the paper. should be close to 0 (eg. 0.001) in order for lambda10 to be slightly > (lambda00 + lambda11 - 1).
+np.random.seed(RANDOM_SEED)
+
+# class Config:
+#     def __init__(self):
+#         self.mu = MU
+#         ...
+#         self.random_seed = RANDOM_SEED
+
+class ClusterManager:
     def __init__(self):
         self.node_name = MPI.Get_processor_name()
         self.intracomm = MPI.COMM_WORLD
@@ -27,10 +53,18 @@ class ClusterManager(object):
         return self.node_rank + 1 == self.cluster_size
 
 
+if __name__ == "__main__":
 
-mgr = ClusterManager()
+    manager = ClusterManager()
 
+    screen_names = ["user1", "user2", "user3"] # TODO: fetch via graph.nodes
+    bot_probabilities = dict.fromkeys(screen_names, 0.5) # no priors, set all at 0.5!
+    print(bot_probabilities) #> {'user1': 0.5, 'user2': 0.5, 'user3': 0.5}
 
+    counter = 0 # to be compared with N_ITERS
+    while counter < N_ITERS:
+        print("DOING STUFF HERE!")
+        counter += 1
 
 
 
@@ -40,118 +74,76 @@ mgr = ClusterManager()
 
 exit()
 
-nproc = intracomm.Get_size() #> 1 in dev
-rank  = intracomm.Get_rank() #> 0 in dev
-
-print(rank != nproc - 1) #> (dev: False)
-print(rank == nproc - 1) #> (dev: True)
-
-##master processor first sends cut info, then aggregates results of local pibots from other processors
-if(rank==nproc-1):
-
-	np.random.seed(SEED)
-	countP=0
-	piBot = dict.fromkeys(all_users,0.5)
-
-	##base mode = no prior = all accounts set to proba bot=0.5 at beginning
-	piBot = dict.fromkeys(all_users,0.5)
-
-	##different modes for different priors.
-	##use botometer scores
-	if(mode=='boto'):
-		boto = readCustomDic_Pibot('botometer_scores/'+db+'_fullBotometer_piBots.csv')
-		med = np.median(list(boto.values()))
-
-		for i in piBot:
-			if(i in boto):
-				piBot[i]=boto[i]
-			else:
-				piBot[i]=med
 
 
-	##random priors
-	elif(mode=='random_unif'):
-		rand=np.random.uniform(0,1,len(piBot))
-		piBot=dict(zip(all_users,rand))
-
-	elif(mode=='random_gauss'):
-		rand=np.random.normal(0.5,0.1,len(piBot))
-		piBot=dict(zip(all_users,rand))
-
-	##############################
-	###### Perform the cut ######
-	##############################
-	while(True and countP < iterations):
-
-		inDeg = G0.in_degree(weight='weight')
-		if(type(inDeg)!=dict):
-			graph_in = dict((x,y) for x, y in inDeg)
-		else:
-			graph_in = inDeg
-
-		outDeg = G0.out_degree(weight='weight')
-		if(type(outDeg)!=dict):
-			graph_out = dict((x,y) for x, y in outDeg)
-		else :
-			graph_out = outDeg
-
-		print("Starting get link data step")
-
-		link_data = getLinkDataRestrained(G0)
-
-		for n in G0.nodes():
-			if n not in graph_in.keys():
-				graph_in[n]=0
-			if n not in graph_out.keys():
-				graph_out[n]=0
-
-		edgelist_data =[(i[0], i[1], psi(i[0],i[1],i[4],graph_in, graph_out,alpha,alambda1,alambda2,epsilon)) for i in link_data]
-		print("tot edgelist", len(edgelist_data))
 
 
-		##ease computations by only keeping edges with non zero weight
-		edgelist_data = [t for t in edgelist_data if sum(t[2]) > 0]
-		print("only > 0 edgelist", len(edgelist_data))
+inDeg = G0.in_degree(weight='weight')
+if(type(inDeg)!=dict):
+    graph_in = dict((x,y) for x, y in inDeg)
+else:
+    graph_in = inDeg
 
-		H, PL, user_data = computeH(G0, piBot, edgelist_data, graph_out, graph_in)
-		print(rank, 'completed graph cut, send it to children')
-		writeCSVFile('./'+db+'_subGraphs/PL_mu_'+str(mu)+'_alpha1_'+str(alpha1)+'_alpha2_'+str(alpha2)+'_lambda1_'+str(alambda1)+'_lambda2_'+str(alambda2)+'_epsilon_'+str(epsilon)+'_mode_'+mode+'.csv',PL)
-		writeCSVFile_H('./'+db+'_subGraphs/H_mu_'+str(mu)+'_alpha1_'+str(alpha1)+'_alpha2_'+str(alpha2)+'_lambda1_'+str(alambda1)+'_lambda2_'+str(alambda2)+'_epsilon_'+str(epsilon)+'_mode_'+mode+'_'+str(0)+'.csv',H)
+outDeg = G0.out_degree(weight='weight')
+if(type(outDeg)!=dict):
+    graph_out = dict((x,y) for x, y in outDeg)
+else :
+    graph_out = outDeg
 
-		##send a flag to children processors to start computing local pibot
-		for i in range(0,nproc-2):
-			comm.send(True, dest=i)
+print("Starting get link data step")
 
-		comm.send(True, dest=nproc-2)
+link_data = getLinkDataRestrained(G0)
 
-		##receive results from children and aggregate
-		print(rank, 'ready to receive')
-		gather=[]
-		for i in range(0,nproc-1):
-			r = comm.recv(source=i)
-			gather.append(r)
-			print(rank, ' received from ', i)
+for n in G0.nodes():
+    if n not in graph_in.keys():
+        graph_in[n]=0
+    if n not in graph_out.keys():
+        graph_out[n]=0
 
-		users = list(user_data.keys())
-
-		clustering =dict.fromkeys(users,0)
-		for user in PL:
-			user_data[user]['clustering'] = 1
-			clustering[user] = 1
-
-		print(rank, ' received ', len(gather), ' local piBots')
-		piBot = {}
-		for d in gather:
-			for user in d:
-				piBot[user] = d[user]
-
-		##write result of aggregation. Clustering= hard score, piBot= continuous score between 0 and 1 (pick threshold)
-		writeCSVFile_piBot('./network_piBots_'+db+'/ntwk_piBot_mu_'+str(mu)+'_alpha1_'+str(alpha1)+'_alpha2_'+str(alpha2)+'_lambda1_'+str(alambda1)+'_lambda2_'+str(alambda2)+'_epsilon_'+str(epsilon)+'_mode_'+mode+'_iteration_'+str(countP)+'_SEED_'+str(SEED)+'.csv', piBot)
-		#writeCSVFile_piBot('./network_piBots_'+db+'/ntwk_clustering_mu_'+str(mu)+'_alpha1_'+str(alpha1)+'_alpha2_'+str(alpha2)+'_lambda1_'+str(alambda1)+'_lambda2_'+str(alambda2)+'_epsilon_'+str(epsilon)+'_mode_'+mode+'_iteration_'+str(countP)+'.csv', clustering)
-		countP += 1
+edgelist_data =[(i[0], i[1], psi(i[0],i[1],i[4],graph_in, graph_out,alpha,alambda1,alambda2,epsilon)) for i in link_data]
+print("tot edgelist", len(edgelist_data))
 
 
-MPI.Finalize()
+##ease computations by only keeping edges with non zero weight
+edgelist_data = [t for t in edgelist_data if sum(t[2]) > 0]
+print("only > 0 edgelist", len(edgelist_data))
+
+H, PL, user_data = computeH(G0, piBot, edgelist_data, graph_out, graph_in)
+print(rank, 'completed graph cut, send it to children')
+writeCSVFile('./'+db+'_subGraphs/PL_mu_'+str(mu)+'_alpha1_'+str(alpha1)+'_alpha2_'+str(alpha2)+'_lambda1_'+str(alambda1)+'_lambda2_'+str(alambda2)+'_epsilon_'+str(epsilon)+'_mode_'+mode+'.csv',PL)
+writeCSVFile_H('./'+db+'_subGraphs/H_mu_'+str(mu)+'_alpha1_'+str(alpha1)+'_alpha2_'+str(alpha2)+'_lambda1_'+str(alambda1)+'_lambda2_'+str(alambda2)+'_epsilon_'+str(epsilon)+'_mode_'+mode+'_'+str(0)+'.csv',H)
+
+##send a flag to children processors to start computing local pibot
+for i in range(0,nproc-2):
+    comm.send(True, dest=i)
+
+comm.send(True, dest=nproc-2)
+
+##receive results from children and aggregate
+print(rank, 'ready to receive')
+gather=[]
+for i in range(0,nproc-1):
+    r = comm.recv(source=i)
+    gather.append(r)
+    print(rank, ' received from ', i)
+
+users = list(user_data.keys())
+
+clustering =dict.fromkeys(users,0)
+for user in PL:
+    user_data[user]['clustering'] = 1
+    clustering[user] = 1
+
+print(rank, ' received ', len(gather), ' local piBots')
+piBot = {}
+for d in gather:
+    for user in d:
+        piBot[user] = d[user]
+
+##write result of aggregation. Clustering= hard score, piBot= continuous score between 0 and 1 (pick threshold)
+writeCSVFile_piBot('./network_piBots_'+db+'/ntwk_piBot_mu_'+str(mu)+'_alpha1_'+str(alpha1)+'_alpha2_'+str(alpha2)+'_lambda1_'+str(alambda1)+'_lambda2_'+str(alambda2)+'_epsilon_'+str(epsilon)+'_mode_'+mode+'_iteration_'+str(countP)+'_SEED_'+str(SEED)+'.csv', piBot)
+#writeCSVFile_piBot('./network_piBots_'+db+'/ntwk_clustering_mu_'+str(mu)+'_alpha1_'+str(alpha1)+'_alpha2_'+str(alpha2)+'_lambda1_'+str(alambda1)+'_lambda2_'+str(alambda2)+'_epsilon_'+str(epsilon)+'_mode_'+mode+'_iteration_'+str(countP)+'.csv', clustering)
+countP += 1
 
 
 # MPI.Finalize()
