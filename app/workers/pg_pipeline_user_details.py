ee247e3122063b12126e1771f93097f4e8468a17
Mike Rossetti <s2t2@users.noreply.github.com>
2020-07-16 13:25:27 -0400

    Clock time notes

--- app/workers/pg_pipeline_user_details.py
+++ app/workers/pg_pipeline_user_details.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
 
     pipeline = Pipeline()
 
-    pipeline.download_user_details()
+    pipeline.download_user_details() # takes about 50 minutes for 3.6M users in batches of 2500
 
     pipeline.report()
