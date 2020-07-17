4383a08ed79d08a2be16bb54250f2bc0c1b5bcaf
Mike Rossetti <s2t2@users.noreply.github.com>
2020-07-17 13:13:06 -0400

    Success

--- app/workers/pg_pipeline_retweeter_details.py
+++ app/workers/pg_pipeline_retweeter_details.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
 
     pipeline = Pipeline()
 
-    pipeline.download_retweeter_details() # takes about __ minutes for 2.7M users in batches of 2500
+    pipeline.download_retweeter_details() # takes about 16 minutes for 2.7M users in batches of 2500
 
     pipeline.report()
