df671a3fd50c0e9c1a10d4b8d46429f2ee145519
Mike Rossetti <s2t2@users.noreply.github.com>
2020-07-22 16:13:46 -0400

    WIP

--- app/analyzers/bq_retweeter_creation_two_topics.py
+++ app/analyzers/bq_retweeter_creation_two_topics.py
@@ -1,11 +1,13 @@
 
 import os
 from pprint import pprint
+import json
 
 import numpy as np
 from scipy.stats import kstest, ks_2samp
 from dotenv import load_dotenv
 
+from app import DATA_DIR
 from app.bq_service import BigQueryService
 from app.workers import to_dt, to_ts, fmt_n
 
@@ -15,21 +17,44 @@ np.random.seed(2020)
 X_TOPIC = os.getenv("X_TOPIC", default="#MAGA")
 Y_TOPIC = os.getenv("Y_TOPIC", default="#ImpeachAndRemove")
 
+KS_RESULTS_DIR = os.path.join(DATA_DIR, "ks_tests")
 
 def to_date(my_ts):
     return to_dt(my_ts).strftime("%Y-%m-%d")
+
+def interpret(ks_test_results, pval_max=0.01):
+    """
+    A utility function to interpret the results of a two-sample KS test.
+    Indicates whether or not to reject the null hypothesis.
+
+    See:
+        https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.kstest.html
+        https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.ks_2samp.html
+
+    Methodology Notes / Docs:
+        Under the null hypothesis, the two distributions are identical.
+        If the KS statistic is small or the p-value is high, then we cannot reject the null hypothesis.
+
+    """
+    if ks_test_results.pvalue <= pval_max:
+        interpretation = "REJECT (DIFF)"
+    else:
+        interpretation = "ACCEPT (SAME)"
+    return interpretation
+
+
 
 if __name__ == "__main__":
 
     x_topic = X_TOPIC.upper()
     y_topic = Y_TOPIC.upper()
+    comparison_id = f"{x_topic.replace(' ', '').lower()}_{y_topic.replace(' ', '').lower()}"
 
     bq = BigQueryService.cautiously_initialized()
 
     # get the users talking about topic x and not y
     # for each user, determine how many times they were talking about topic x and y
     # then label the user x, y, neither, or both
-
     sql = f"""
 
         SELECT
@@ -50,6 +75,7 @@ if __name__ == "__main__":
 
     print("-----------------------------")
     print("FETCHING USERS...")
+
     for row in bq.execute_query(sql):
         ts = to_ts(row.user_created_at)
 
@@ -67,6 +93,13 @@ if __name__ == "__main__":
     print("BOTH:", fmt_n(len(both)))
     print("NEITHER:", fmt_n(len(neither)))
 
+    # If the mode is "auto", the computation is exact if the sample sizes are less than 10,000.
+    # ... For larger sizes, the computation uses the KS distributions to compute an approximate value.
+    #
+    # The ‘two-sided’ ‘exact’ computation computes the complementary probability and then subtracts from 1.
+    # ... As such, the minimum probability it can return is about 1e-16.
+    # ... While the algorithm itself is exact, numerical errors may accumulate for large sample sizes. It is most suited to situations in which one of the sample sizes is only a few thousand.
+
     print("-----------------------------")
     print("COMPARING CREATION DATES...")
     x_avg, y_avg = np.mean(x), np.mean(y)
@@ -77,34 +110,36 @@ if __name__ == "__main__":
     print("BOTH:", to_date(both_avg))
     print("NEITHER:", to_date(neither_avg))
 
-    print("TWO-SAMPLE KS TEST...")
+    print("-----------------------------")
+    print("TWO-SAMPLE KS TESTS...")
+
     xy_result = ks_2samp(x, y)
     xn_result = ks_2samp(x, neither)
     yn_result = ks_2samp(y, neither)
     xb_result = ks_2samp(x, both)
     yb_result = ks_2samp(y, both)
 
-    # Under the null hypothesis, the two distributions are identical
-    # If the KS statistic is small or the p-value is high, then we cannot reject the hypothesis that the distributions of the two samples are the same.
-
-    comparison_id = f"{x_topic.replace(' ', '').lower()}_vs_{y_topic.replace(' ', '').lower()}"
     results = {
         "id": comparison_id,
         "topics": {"x": X_TOPIC, "y": Y_TOPIC},
         "samples": {
-            "x": {"size": len(x), "avg": int(x_avg), "avg_date": to_date(x_avg)},
-            "y": {"size": len(y), "avg": int(y_avg), "avg_date": to_date(y_avg)},
-            "both": {"size": len(x), "avg": int(both_avg), "avg_date": to_date(both_avg)},
+            "x": {"size": len(x),       "avg": int(x_avg),      "avg_date": to_date(x_avg)},
+            "y": {"size": len(y),       "avg": int(y_avg),      "avg_date": to_date(y_avg)},
+            "both": {"size": len(x),    "avg": int(both_avg),   "avg_date": to_date(both_avg)},
             "neither": {"size": len(x), "avg": int(neither_avg), "avg_date": to_date(neither_avg)},
         },
         "results": {
-            "x vs y":       {"stat": xy_result.statistic, "pval": xy_result.pvalue}, # "interpretation":"REJECT"
-            "x vs neither": {"stat": xn_result.statistic, "pval": xn_result.pvalue}, # "interpretation":"REJECT"
-            "y vs neither": {"stat": yn_result.statistic, "pval": yn_result.pvalue}, # "interpretation":"REJECT"
-            "x vs both":    {"stat": xb_result.statistic, "pval": xb_result.pvalue}, # "interpretation":"REJECT"
-            "y vs both":    {"stat": yb_result.statistic, "pval": yb_result.pvalue}, # "interpretation":"REJECT"
+            "x-y":       {"stat": xy_result.statistic, "pval": xy_result.pvalue, "interpretation": interpret(xy_result)},
+            "x-neither": {"stat": xn_result.statistic, "pval": xn_result.pvalue, "interpretation": interpret(xn_result)},
+            "y-neither": {"stat": yn_result.statistic, "pval": yn_result.pvalue, "interpretation": interpret(yn_result)},
+            "x-both":    {"stat": xb_result.statistic, "pval": xb_result.pvalue, "interpretation": interpret(xb_result)},
+            "y-both":    {"stat": yb_result.statistic, "pval": yb_result.pvalue, "interpretation": interpret(yb_result)},
         }
     }
     pprint(results)
 
-    breakpoint()
+    print("WRITING TO FILE...")
+    json_filepath = os.path.join(KS_RESULTS_DIR, f"{comparison_id}.json")
+    print(json_filepath)
+    with open(json_filepath, "w") as f:
+        json.dump(results, f)
