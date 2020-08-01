445a253f66f004489f1010de7241a019f0ed49c5
Mike Rossetti <s2t2@users.noreply.github.com>
2020-08-01 09:23:33 -0400

    Theoretical refactor of friend collector into OOP

--- app/friend_collection/__init__.py
+++ app/friend_collection/__init__.py
@@ -11,12 +11,12 @@ from app.friend_collection.twitter_scraper import get_friends, VERBOSE_SCRAPER, 
 
 load_dotenv()
 
-MAX_THREADS = int(os.getenv("MAX_THREADS", default=10)) # the max number of threads to use, for concurrent processing
-BATCH_SIZE = int(os.getenv("BATCH_SIZE", default=20)) # the max number of processed users to store in BQ at once (with a single insert API call)
+MIN_USER_ID = os.getenv("MIN_USER_ID") # if partitioning users, the lower bound of the partition
+MAX_USER_ID = os.getenv("MAX_USER_ID") # if partitioning users, the upper bound of the partition
+USERS_LIMIT = os.getenv("USERS_LIMIT") # max number of users to fetch from the db
 
-MIN_ID = os.getenv("MIN_USER_ID") # if partitioning users, the lower bound of the partition
-MAX_ID = os.getenv("MAX_USER_ID") # if partitioning users, the upper bound of the partition
-LIMIT = os.getenv("USERS_LIMIT") # max number of users to fetch from the db
+MAX_THREADS = int(os.getenv("MAX_THREADS", default=3)) # the max number of threads to use, for concurrent processing
+BATCH_SIZE = int(os.getenv("BATCH_SIZE", default=20)) # the max number of processed users to store in BQ at once (with a single insert API call)
 
 #VERBOSE_COLLECTOR = os.getenv("VERBOSE_COLLECTOR", default="true") == "true"
 
@@ -37,56 +37,62 @@ def user_with_friends(row):
         "end_at": end_at
     }
 
-def cautiously_initialized_storage_service():
-    service = BigQueryService()
-    print("-------------------------")
-    print("BQ CONFIG...")
-    print("  BIGQUERY DATASET:", service.dataset_address.upper())
-    print("  DESTRUCTIVE MIGRATIONS:", service.destructive)
-    print("  VERBOSE QUERIES:", service.verbose)
-    print("-------------------------")
-    print("WORKER CONFIG...")
-    print("  MIN USER ID:", MIN_ID)
-    print("  MAX USER ID:", MAX_ID)
-    print("  USERS LIMIT:", LIMIT)
-    print("  MAX THREADS:", MAX_THREADS)
-    print("  BATCH SIZE:", BATCH_SIZE)
-    print("-------------------------")
-    print("SCRAPER CONFIG...")
-    print("  VERBOSE SCRAPER:", VERBOSE_SCRAPER)
-    print("  MAX FRIENDS:", MAX_FRIENDS)
-    print("-------------------------")
+class FriendCollector:
+    def __init__(self, min_user_id=MIN_USER_ID, max_user_id=MAX_USER_ID, users_limit=USERS_LIMIT, max_threads=MAX_THREADS, batch_size=BATCH_SIZE):
+        self.bq = BigQueryService.cautiously_initialized()
+
+        self.min_user_id = min_user_id # OK to be None
+        self.max_user_id = max_user_id # OK to be None
+        self.users_limit = users_limit
+
+        self.max_threads = max_threads
+        self.batch_size = batch_size
+
+        print("-------------------------")
+        print("FRIEND COLLECTOR CONFIG...")
+        print("  MIN USER ID:", self.min_user_id)
+        print("  MAX USER ID:", self.max_user_id)
+        print("  USERS LIMIT:", self.users_limit)
+        print("  MAX THREADS:", self.max_threads)
+        print("  BATCH SIZE:", self.batch_size)
+        print("-------------------------")
+        print("SCRAPER CONFIG...")
+        print("  VERBOSE SCRAPER:", VERBOSE_SCRAPER)
+        print("  MAX FRIENDS:", MAX_FRIENDS)
+        print("-------------------------")
+
+    def perform(self):
+        users = self.bq.fetch_remaining_users(min_id=self.min_user_id, max_id=self.max_user_id, limit=self.users_limit)
+        print("FETCHED UNIVERSE OF", len(users), "USERS")
+
+        with ThreadPoolExecutor(max_workers=self.max_threads, thread_name_prefix="THREAD") as executor:
+            batch = []
+            lock = BoundedSemaphore()
+            futures = [executor.submit(user_with_friends, row) for row in users]
+            print("FUTURE RESULTS", len(futures))
+            for index, future in enumerate(as_completed(futures)):
+                result = future.result()
+
+                # OK, so this locking business:
+                # ... prevents random threads from clearing the batch, which was causing results to almost never get stored, and
+                # ... restricts a thread's ability to acquire access to the batch until another one has released it
+                lock.acquire()
+                batch.append(result)
+                if (len(batch) >= self.batch_size) or (index + 1 >= len(futures)): # when batch is full or is last
+                    print(f"SAVING BATCH OF {len(batch)}...")
+                    self.bq.insert_user_friends(batch)
+                    print("CLEARING BATCH...")
+                    batch = []
+                lock.release()
+
+
+if __name__ == "__main__":
+
+    collector = FriendCollector()
+
     if APP_ENV == "development":
         if input("CONTINUE? (Y/N): ").upper() != "Y":
             print("EXITING...")
             exit()
-    #service.init_tables() # did this originally, but moving to a manual migration strategy to prevent accidental deletions
-    return service
 
-if __name__ == "__main__":
-
-    service = cautiously_initialized_storage_service()
-
-    users = service.fetch_remaining_users(min_id=MIN_ID, max_id=MAX_ID, limit=LIMIT)
-    print("FETCHED UNIVERSE OF", len(users), "USERS")
-
-    with ThreadPoolExecutor(max_workers=MAX_THREADS, thread_name_prefix="THREAD") as executor:
-        batch = []
-        lock = BoundedSemaphore()
-        futures = [executor.submit(user_with_friends, row) for row in users]
-        print("FUTURE RESULTS", len(futures))
-        for index, future in enumerate(as_completed(futures)):
-            result = future.result()
-
-            # OK, so this locking business:
-            # ... prevents random threads from clearing the batch, which was causing results to almost never get stored, and
-            # ... restricts a thread's ability to acquire access to the batch until another one has released it
-            lock.acquire()
-            batch.append(result)
-            if (len(batch) >= BATCH_SIZE) or (index + 1 >= len(futures)): # when batch is full or is last
-                print("-------------------------")
-                print(f"SAVING BATCH OF {len(batch)}...")
-                print("-------------------------")
-                service.insert_user_friends(batch)
-                batch = []
-            lock.release()
+    collector.perform()
