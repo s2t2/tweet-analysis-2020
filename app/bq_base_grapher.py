491c891da341d1b0adb430bef5772c526874b158
Mike Rossetti <s2t2@users.noreply.github.com>
2020-08-01 15:14:55 -0400

    Add a more generic / abstract bigquery base grapher class

---
+++ app/bq_base_grapher.py
@@ -0,0 +1,27 @@
+
+from app.base_grapher import BaseGrapher
+from app.bq_service import BigQueryService
+
+class BigQueryBaseGrapher(BaseGrapher):
+
+    def __init__(self, bq_service=None, gcs_service=None):
+        super().__init__(gcs_service=gcs_service)
+        self.bq_service = (bq_service or BigQueryService.cautiously_initialized())
+
+    @property
+    def metadata(self):
+        return {**super().metadata, **self.bq_service.metadata} # merges dicts
+
+if __name__ == "__main__":
+
+    grapher = BigQueryBaseGrapher()
+
+    # print(type(grapher))
+    # print(type(grapher.bq_service))
+    # print(type(grapher.gcs_service))
+    # print(dir(grapher))
+
+    grapher.start()
+    grapher.perform()
+    grapher.end()
+    grapher.report()
---
+++ app/retweet_graphs/bq_weekly_grapher.py
@@ -0,0 +1,14 @@
+
+
+from app.bq_base_grapher import BigQueryBaseGrapher
+
+class BigQueryWeeklyRetweetGrapher:
+
+    #def perform(self):
+    #    print(self.full_weeks)
+
+
+if __name__ == "__main__":
+    grapher = BigQueryWeeklyRetweetGrapher()
+
+    #grapher.perform()
