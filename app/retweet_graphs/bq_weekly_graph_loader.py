7cc035882935517ec7521885bb4b169a105ae6f2
Mike Rossetti <s2t2@users.noreply.github.com>
2020-08-03 19:55:59 -0400

    Fix graph reporting

--- app/retweet_graphs/bq_weekly_graph_loader.py
+++ app/retweet_graphs/bq_weekly_graph_loader.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
 
     storage_service = BigQueryWeeklyRetweetGrapher.__init_storage_service__()
 
-    storage_service.load_graph() # will print a memory profile...
+    graph = storage_service.load_graph() # will print a memory profile...
 
-    storage_service.report() # will print graph size
+    storage_service.report(graph) # will print graph size
