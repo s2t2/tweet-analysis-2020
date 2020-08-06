e7018e770dc0367afc2a1cbe7e7a04620e8c864f
Mike Rossetti <s2t2@users.noreply.github.com>
2020-08-06 13:00:25 -0400

    Integration testing on development

--- app/retweet_graphs_v2/lookup_user_ids.py
+++ app/retweet_graphs_v2/lookup_user_ids.py
@@ -13,21 +13,24 @@ from app.twitter_service import TwitterService
 
 load_dotenv()
 
-BATCH_SIZE = int(os.getenv("BATCH_SIZE", default=1000)) # the max number of processed users to store in BQ at once (with a single insert API call). must be less than 10,000 to avoid error.
+BATCH_SIZE = int(os.getenv("BATCH_SIZE", default=100)) # the max number of processed users to store in BQ at once (with a single insert API call). must be less than 10,000 to avoid error.
 
 if __name__ == "__main__":
-
-    print("BATCH SIZE:", BATCH_SIZE)
 
     bq_service = BigQueryService()
     twitter_service = TwitterService()
 
+    rows = list(bq_service.fetch_idless_screen_names())
+    row_count = len(rows)
+    print("-------------------------")
+    print(f"FETCHED {row_count} SCREEN NAMES")
+    print("BATCH SIZE:", BATCH_SIZE)
+    print("-------------------------")
+
+    seek_confirmation()
     bq_service.migrate_user_id_lookups()
 
     batch = []
-    rows = list(bq_service.fetch_idless_screen_names())
-    row_count = len(rows)
-    print(f"FETCHED {row_count} SCREEN NAMES")
     for index, row in enumerate(rows):
         counter = index + 1
 
@@ -45,18 +48,19 @@ if __name__ == "__main__":
         print(lookup)
         batch.append(lookup)
 
-        if (counter % BATCH_SIZE == 0) or (counter >= row_count): # if the batch is full or the row is last
+        if (len(batch) >= BATCH_SIZE) or (counter >= row_count): # if the batch is full or the row is last
             print("SAVING BATCH...")
             bq_service.upload_user_id_lookups(batch)
+            batch = [] # clear the batch
 
     print("-------------")
     print("LOOKUPS COMPLETE!")
 
-    print("WRITING TO CSV...")
-    df = DataFrame(lookups)
-    print(df.head())
-    csv_filepath = os.path.join(DATA_DIR, "user_id_lookups.csv")
-    df.to_csv(csv_filepath, index=False)
+    #print("WRITING TO CSV...")
+    #df = DataFrame(lookups)
+    #print(df.head())
+    #csv_filepath = os.path.join(DATA_DIR, "user_id_lookups.csv")
+    #df.to_csv(csv_filepath, index=False)
 
     # google.api_core.exceptions.BadRequest: 400 POST https://bigquery.googleapis.com/bigquery/v2/projects/tweet-collector-py/datasets/impeachment_production/tables/user_id_lookups/insertAll: too many rows present in the request, limit: 10000 row count: 17196.
     #bq_service.upload_user_id_lookups(lookups[0:9000])
