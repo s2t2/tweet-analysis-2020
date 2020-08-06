48b1f3bbc5cc3839a92830dc22241f1a8b768873
Mike Rossetti <s2t2@users.noreply.github.com>
2020-08-06 00:26:24 -0400

    Handle lookup errors

--- app/retweet_graphs_v2/lookup_user_ids.py
+++ app/retweet_graphs_v2/lookup_user_ids.py
@@ -1,4 +1,6 @@
 
+import json
+from tweepy.error import TweepError
 
 from app.bq_service import BigQueryService
 from app.twitter_service import TwitterService
@@ -11,8 +13,20 @@ if __name__ == "__main__":
 
     batch = []
     for row in bq_service.fetch_idless_screen_names():
-        user_id = twitter_service.get_user_id(row.screen_name)
-        lookup = {"screen_name": row.screen_name.upper(), "user_id": user_id}
+
+        try:
+            #print(row.screen_name)
+            user_id = twitter_service.get_user_id(row.screen_name)
+            message = None
+        except TweepError as err:
+            #print(err)
+            #> [{'code': 50, 'message': 'User not found.'}]
+            #> [{'code': 63, 'message': 'User has been suspended.'}]
+            #print("USER SUSPENDED!")
+            user_id = None
+            message = json.loads(err.reason.replace("'", '"'))[0]["message"]
+
+        lookup = {"screen_name": row.screen_name.upper(), "user_id": user_id, "message": message}
         print(lookup)
         batch.append(lookup)
 
