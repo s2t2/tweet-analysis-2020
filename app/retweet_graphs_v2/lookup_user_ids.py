8025af4f7278847c607c930feaed93b85d5960a5
Mike Rossetti <s2t2@users.noreply.github.com>
2020-08-06 00:37:04 -0400

    Write lookups to CSV

--- app/retweet_graphs_v2/lookup_user_ids.py
+++ app/retweet_graphs_v2/lookup_user_ids.py
@@ -1,7 +1,11 @@
 
+import os
 import json
-from tweepy.error import TweepError
 
+from tweepy.error import TweepError
+from pandas import DataFrame
+
+from app import DATA_DIR
 from app.bq_service import BigQueryService
 from app.twitter_service import TwitterService
 
@@ -11,23 +15,28 @@ if __name__ == "__main__":
     bq_service = BigQueryService()
     twitter_service = TwitterService()
 
-    batch = []
+    counter = 1
+    lookups = []
     for row in bq_service.fetch_idless_screen_names():
 
         try:
-            #print(row.screen_name)
             user_id = twitter_service.get_user_id(row.screen_name)
             message = None
         except TweepError as err:
             #print(err)
             #> [{'code': 50, 'message': 'User not found.'}]
             #> [{'code': 63, 'message': 'User has been suspended.'}]
-            #print("USER SUSPENDED!")
             user_id = None
             message = json.loads(err.reason.replace("'", '"'))[0]["message"]
 
-        lookup = {"screen_name": row.screen_name.upper(), "user_id": user_id, "message": message}
+        lookup = {"counter": counter, "screen_name": row.screen_name.upper(), "user_id": user_id, "message": message}
         print(lookup)
-        batch.append(lookup)
+        lookups.append(lookup)
+        counter+=1
 
-    breakpoint()
+    print("-------------")
+    print("LOOKUPS COMPLETE!")
+    df = DataFrame(lookups)
+    print(df.head())
+    csv_filepath = os.path.join(DATA_DIR, "user_id_lookups.csv")
+    df.to_csv(csv_filepath, index=False)
