6c06b55b359b3aa34368b06876d67b83dc6c3e84
Mike Rossetti <s2t2@users.noreply.github.com>
2020-08-12 23:12:38 -0400

    Hotfix - Classify without uploading bot probabilities to BigQuery (#37)
    * Maybe split into batches before trying to save. Getting to the end to reach a bigquery error is such a buzz kill
    
    * Let's just reduce scope and get the CSV files later

--- app/retweet_graphs_v2/k_days/classifier.py
+++ app/retweet_graphs_v2/k_days/classifier.py
@@ -1,11 +1,17 @@
 
 import os
+#import time
 
 from app import APP_ENV, server_sleep
 from app.retweet_graphs_v2.graph_storage import GraphStorage
 from app.retweet_graphs_v2.k_days.generator import DateRangeGenerator
 from app.botcode_v2.classifier import NetworkClassifier as BotClassifier
 from app.bq_service import BigQueryService
+
+#def split_into_batches(my_list, batch_size=9000):
+#    """h/t: https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks"""
+#    for i in range(0, len(my_list), batch_size):
+#        yield my_list[i : i + batch_size]
 
 if __name__ == "__main__":
 
@@ -19,10 +25,9 @@ if __name__ == "__main__":
         storage.report() # loads graph and provides size info
 
         clf = BotClassifier(storage.graph, weight_attr="weight")
-        df = clf.bot_probabilities_df
 
         # UPLOAD COMPLETE CSV TO GOOGLE CLOUD STORAGE
-        df.to_csv(storage.local_bot_probabilities_filepath)
+        clf.bot_probabilities_df.to_csv(storage.local_bot_probabilities_filepath)
         storage.upload_bot_probabilities()
 
         # UPLOAD COMPLETE HISTOGRAM TO GOOGLE CLOUD STORAGE
@@ -33,18 +38,25 @@ if __name__ == "__main__":
         )
         storage.upload_bot_probabilities_histogram()
 
-        # UPLOAD SELECTED ROWS TO BIG QUERY
-        bots_df = df[df["bot_probability"] > 0.5]
-        records = [{**{"start_date": date_range.start_date}, **record} for record in bots_df.to_dict("records")]
-        print("UPLOADING", len(records), "BOT SCORES TO BQ...")
-        bq_service.upload_daily_bot_probabilities(records)
+        ## UPLOAD SELECTED ROWS TO BIG QUERY
+        #df = clf.bot_probabilities_df
+        #bots_df = df[df["bot_probability"] > 0.5]
+        #records = [{**{"start_date": date_range.start_date}, **record} for record in bots_df.to_dict("records")]
+        #print("UPLOADING", len(records), "BOT SCORES TO BQ...")
+        ##bq_service.upload_daily_bot_probabilities(records)
+        ##> google.api_core.exceptions.BadRequest: 400 POST https://bigquery.googleapis.com/bigquery/v2/projects/.../tables/daily_bot_probabilities/insertAll: too many rows present in the request, limit: 10000 row count: 36092.
+        ##batches = list(split_into_batches(records))
+        ##for batch in batches:
+        ##    bq_service.upload_daily_bot_probabilities(batch)
+        ##    time.sleep(5)
 
         # CLEAR MEMORY
         del storage
         del clf
-        del df
-        del bots_df
-        del records
+        #del df
+        #del bots_df
+        #del records
+        #del batches
         print("\n\n\n\n")
 
     print("JOB COMPLETE!")
