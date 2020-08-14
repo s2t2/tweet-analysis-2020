32b3629447fa2913d7b3ad9880beb4a977295160
MJ Rossetti <s2t2@users.noreply.github.com>
2020-08-14 09:52:41 -0400

    Update test

--- test/test_k_days.py
+++ test/test_k_days.py
@@ -1,10 +1,12 @@
 
 from datetime import datetime
 
-from app.retweet_graphs_v2.k_days.grapher import get_date_ranges
+from app.retweet_graphs_v2.k_days.generator import DateRangeGenerator
 
 def test_date_ranges():
-    assert get_date_ranges(start_date="2020-01-01", k_days=3, n_periods=5) == [
+
+    gen = DateRangeGenerator(start_date="2020-01-01", k_days=3, n_periods=5)
+    assert [{"start_at": dr.start_at, "end_at": dr.end_at} for dr in gen.date_ranges] == [
         {'start_at': datetime(2020, 1, 1, 0, 0),  'end_at': datetime(2020, 1, 3, 23, 59, 59)},
         {'start_at': datetime(2020, 1, 4, 0, 0),  'end_at': datetime(2020, 1, 6, 23, 59, 59)},
         {'start_at': datetime(2020, 1, 7, 0, 0),  'end_at': datetime(2020, 1, 9, 23, 59, 59)},
