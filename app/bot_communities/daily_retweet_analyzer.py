13fb341db747283206467c1fab894f12e5c25df8
MJ Rossetti <s2t2@users.noreply.github.com>
2020-08-25 00:01:56 -0400

    Pass filtered dataframe to parallel process

--- app/bot_communities/daily_retweet_analyzer.py
+++ app/bot_communities/daily_retweet_analyzer.py
@@ -6,13 +6,13 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 #from threading import current_thread #, #Thread, Lock, BoundedSemaphore
 
 from dotenv import load_dotenv
-from pandas import DataFrame
+from pandas import DataFrame, to_datetime
 import matplotlib.pyplot as plt
 import plotly.express as px
 import squarify
 
 from app import APP_ENV, seek_confirmation
-from app.decorators.datetime_decorators import logstamp
+from app.decorators.datetime_decorators import logstamp, s_to_date
 from app.decorators.number_decorators import fmt_n
 from app.bot_communities.csv_storage import LocalStorage
 from app.bot_communities.retweet_analyzer import RetweetsAnalyzer
@@ -22,11 +22,21 @@ from app.bot_communities.token_analyzer import summarize_token_frequencies, trai
 
 load_dotenv()
 
-MAX_THREADS = int(os.getenv("MAX_THREADS", default=3))
+START_DATE = os.getenv("START_DATE", default="2019-12-12")
+END_DATE = os.getenv("END_DATE", default="2020-02-20")
 
-def perform(community_id, date):
-    print(f"COMMUNITY ID: {community_id} | DATE: {date}")
+MAX_THREADS = int(os.getenv("MAX_THREADS", default=10))
 
+#def perform(community_id, date):
+#    print(f"COMMUNITY ID: {community_id} | DATE: {date}")
+
+def perform(group_name, filtered_df):
+    community_id = group_name[0]
+    date = group_name[1]
+    print("----------------")
+    print(logstamp(), "COMMUNITY", community_id, "| DATE:", date, "| RETWEETS:", fmt_n(len(filtered_df)))
+
+    time.sleep(3)
 
     #community_analyzer = DailyCommunityRetweetsAnalyzer(
     #    community_id=community_id,
@@ -53,17 +63,41 @@ def perform(community_id, date):
 
 if __name__ == "__main__":
 
+    print("------------------------")
+    print("DAILY COMMUNITY RETWEETS ANALYSIS...")
+
     storage = LocalStorage()
     storage.load_retweets()
-    print(storage.retweets_df.head())
+    df = storage.retweets_df
+    print(df.head())
+
+    seek_confirmation()
+
+    print("----------------")
+    print("TRANSFORMING RETWEETS...")
+    df["status_created_date"] = df["status_created_at"].apply(s_to_date)
+    df["status_created_dt"] = to_datetime(df["status_created_at"])
+
+    print("----------------")
+    print("FILTERING RETWEETS...")
+    print("  START DATE", START_DATE)
+    print("  END DATE", END_DATE)
+    df = df.query(f"{START_DATE.replace('-','')} < status_created_dt < {END_DATE.replace('-','')}") # dashes are for humans, not dataframe queries apparently
+    print(df["status_created_date"].value_counts())
 
     seek_confirmation()
 
     with ProcessPoolExecutor(max_workers=MAX_THREADS) as executor:
 
-        date = "2020-01-01"
-        futures = [executor.submit(perform, community_id, date) for community_id in storage.retweet_community_ids]
+        #date = "2020-01-01"
+        #futures = [executor.submit(perform, community_id, date) for community_id in storage.retweet_community_ids]
+        #for future in as_completed(futures):
+        #    result = future.result()
+        #    #print(result)
+
+        groupby = storage.retweets_df.groupby(["community_id", "status_created_date"])
+
+        futures = [executor.submit(perform, group_name, filtered_df) for group_name, filtered_df in groupby]
 
         for future in as_completed(futures):
             result = future.result()
-            #print(result)
