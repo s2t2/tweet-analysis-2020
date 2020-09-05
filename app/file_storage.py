219c3f75171f5f2c2a4d69ce331dad2d067d7a5c
MJ Rossetti <s2t2@users.noreply.github.com>
2020-09-05 18:15:42 -0400

    Refactor model storage service

---
+++ app/file_storage.py
@@ -0,0 +1,57 @@
+
+import os
+from pprint import pprint
+
+from dotenv import load_dotenv
+
+from app import DATA_DIR, seek_confirmation
+from app.decorators.datetime_decorators import logstamp
+from app.gcs_service import GoogleCloudStorageService
+
+load_dotenv()
+
+DIRPATH = os.getenv("DIRPATH", default="example/file_storage")
+WIFI = (os.getenv("WIFI", default="true") == "true")
+
+class FileStorage:
+
+    def __init__(self, dirpath=None, gcs_service=None, wifi=WIFI):
+        """
+        Saves and loads files, using local storage and/or Google Cloud Storage.
+
+        Params:
+            dirpath (str) a subpath of the data dir
+            wifi (bool) whether or not to attempt uploads
+
+        """
+        self.wifi = wifi
+        self.gcs_service = gcs_service or GoogleCloudStorageService()
+
+        self.dirpath = dirpath or DIRPATH
+        self.gcs_dirpath = os.path.join("storage", "data", self.dirpath)
+        self.local_dirpath = os.path.join(DATA_DIR, self.dirpath) # TODO: to make compatible on windows, split the dirpath on "/" and re-join using os.sep
+
+        print("-------------------------")
+        print("FILE STORAGE...")
+        print("   DIRPATH:",  self.dirpath)
+        print("   GCS DIRPATH:", self.gcs_dirpath)
+        print("   LOCAL DIRPATH:", os.path.abspath(self.local_dirpath))
+        print("   WIFI ENABLED:", self.wifi)
+
+        seek_confirmation()
+
+        if not os.path.exists(self.local_dirpath):
+            os.makedirs(self.local_dirpath)
+
+    #
+    # REMOTE STORAGE
+    #
+
+    def upload_file(self, local_filepath, remote_filepath):
+        print(logstamp(), "UPLOADING FILE...", os.path.abspath(local_filepath))
+        blob = self.gcs_service.upload(local_filepath, remote_filepath)
+        print(logstamp(), blob) #> <Blob: impeachment-analysis-2020, storage/data/2020-05-26-0002/metadata.json, 1590465770194318>
+
+    def download_file(self, remote_filepath, local_filepath):
+        print(logstamp(), "DOWNLOADING FILE...", remote_filepath)
+        self.gcs_service.download(remote_filepath, local_filepath)
--- app/nlp/model_storage.py
+++ app/nlp/model_storage.py
@@ -1,32 +1,72 @@
 import os
 import pickle
 
+from memory_profiler import profile
+
 from app import DATA_DIR
+from app.file_storage import FileStorage
 
-MODELS_DIRPATH = os.path.join(DATA_DIR, "user_communities", "n_communities", str(2), "tweet_classifier", "models")
+MODELS_DIRPATH = os.path.join(DATA_DIR, "tweet_classifier", "models")
 
-def save_model(model, local_filepath):
-    print("SAVING MODEL TO LOCAL FILE...")
-    with open(local_filepath, "wb") as f:
-        pickle.dump(model, f)
+class ModelStorage(FileStorage):
+    def  __init__(self, dirpath=MODELS_DIRPATH, model_name="model.gpickle"):
+        super().__init__(dirpath=dirpath)
+        self.local_model_filepath = os.path.join(self.local_dirpath, model_name)
+        self.gcs_model_filepath = os.path.join(self.gcs_dirpath, model_name)
 
-def load_model(local_filepath):
-    print("LOADING MODEL FROM LOCAL FILE...")
-    with open(local_filepath, "rb") as f:
-        model = pickle.load(f)
-    return model
+    #
+    # LOCAL STORAGE
+    #
+
+    def write_model(self, model):
+        print("WRITING MODEL TO LOCAL FILE...")
+        with open(self.local_model_filepath, "wb") as f:
+            pickle.dump(model, f)
+
+    def read_model(self):
+        print("READING MODEL FROM LOCAL FILE...")
+        with open(self.local_model_filepath, "rb") as f:
+            return pickle.load(f)
+
+    #
+    # REMOTE STORAGE
+    #
+
+    def upload_model(self):
+        self.upload_file(self.local_model_filepath, self.gcs_model_filepath)
+
+    def download_model(self):
+        self.upload_file(self.gcs_model_filepath, self.local_model_filepath)
+
+    #
+    # CONVENIENCE METHODS
+    #
+
+    def save_model(self, model):
+        self.write_model(model)
+
+        if self.wifi:
+            self.upload_model()
+
+    @profile
+    def load_model(self):
+        """Assumes the graph already exists and is saved locally or remotely"""
+        if not os.path.isfile(self.local_model_filepath):
+            self.download_model()
+
+        return self.read_model()
 
 
 if __name__ == "__main__":
 
     from sklearn.linear_model import LogisticRegression
 
-    clf = LogisticRegression()
-    print(type(clf))
+    example_dirpath = os.path.join(MODELS_DIRPATH, "example")
+    storage = ModelStorage(dirpath=example_dirpath)
 
-    local_filepath = os.path.join(MODELS_DIRPATH, "example.gpickle")
+    model = LogisticRegression()
+    print(type(model))
+    storage.save_model(model)
 
-    save_model(clf, local_filepath)
-
-    clf = load_model(local_filepath)
-    print(type(clf))
+    same_model = storage.load_model()
+    print(type(same_model))
