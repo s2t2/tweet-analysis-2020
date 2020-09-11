af8f6270d33493c2ed4da01893ef8550b279a786
MJ Rossetti <s2t2@users.noreply.github.com>
2020-09-11 16:32:04 -0400

    Bootstrap layout with basic routing

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,21 +1,25 @@
 # web_app/routes/home_routes.py
 
-from flask import Blueprint #, render_template
+from flask import Blueprint, render_template
 
 home_routes = Blueprint("home_routes", __name__)
 
 @home_routes.route("/")
 def index():
     print("VISITED THE HOME PAGE")
-    #return render_template("dashboard.html")
-    return "Welcome Home (TODO)"
+    return render_template("home.html")
 
 @home_routes.route("/about")
 def about():
     print("VISITED THE ABOUT PAGE")
-    return "About Me (TODO)"
+    return render_template("about.html")
 
-@home_routes.route("/register")
-def register():
-    print("VISITED THE REGISTRATION PAGE")
-    return "Sign Up for our Product! (TODO)"
+@home_routes.route("/events")
+def events():
+    print("VISITED THE EVENTS PAGE")
+    return render_template("events.html")
+
+@home_routes.route("/topics")
+def topics():
+    print("VISITED THE TOPICS PAGE")
+    return render_template("topics.html", event_name="impeachment")
