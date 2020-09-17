e84603bcea0e37234b1b0aeba5b29f9f3dbab2e5
MJ Rossetti <s2t2@users.noreply.github.com>
2020-09-17 13:33:06 -0400

    Make a plan

---
+++ app/bot_impact/README.md
@@ -0,0 +1,7 @@
+# Bot Impact Analysis
+
+## Usage
+
+```sh
+DATE="2020-02-05" python -m app.bot_impact.daily_friend_grapher
+```
---
+++ app/bot_impact/daily_friend_grapher.py
@@ -0,0 +1,10 @@
+
+
+
+if __name__ == "__main__":
+
+    # for all tweets on a given day,
+    # assemble a friend graph between all tweeters and the people they follow
+    # edge format: (follower, following1)
+
+    # and save to gpickle and upload to GCS
