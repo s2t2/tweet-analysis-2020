13157dd9c5f2ae7c4b8ed622650ae7c7f30f3ac8
MJ Rossetti <s2t2@users.noreply.github.com>
2020-09-29 12:24:48 -0400

    Upload files to GCS

--- app/bot_communities/bot_analyzer_v2.py
+++ app/bot_communities/bot_analyzer_v2.py
@@ -2,18 +2,18 @@ import os
 
 from app import DATA_DIR
 from app.bq_service import BigQueryService
-#from app.file_storage import FileStorage
+from app.file_storage import FileStorage
 from app.bot_communities.tokenizers import Tokenizer #, SpacyTokenizer
 from app.bot_communities.token_analyzer import summarize_token_frequencies
 
 from pandas import DataFrame
 
+
 if __name__ == "__main__":
 
-    local_dirpath = os.path.join(DATA_DIR, "bot_retweet_graphs", "bot_min", str(0.8), "n_communities", str(2))
-    profiles_filepath = os.path.join(local_dirpath, "tokenized_profiles.csv")
-
+    file_storage = FileStorage(dirpath="bot_retweet_graphs/bot_min/0.8/n_communities/2/analysis_v2")
     bq_service = BigQueryService()
+
     results = [dict(row) for row in list(bq_service.fetch_bot_community_profiles(n_communities=2))]
     print("PROCESSING", len(results), "RECORDS...")
 
@@ -36,25 +36,34 @@ if __name__ == "__main__":
             row["profile_tags"] = tags
             #print("TAGS:", tags)
 
-    df = DataFrame(results)
-    df.to_csv(profiles_filepath)
-    print(df.head())
+    profiles_df = DataFrame(results)
+    print(profiles_df.head())
+    local_profiles_filepath = os.path.join(file_storage.local_dirpath, "community_profiles.csv")
+    gcs_profiles_filepath = os.path.join(file_storage.gcs_dirpath, "community_profiles.csv")
+    profiles_df.to_csv(local_profiles_filepath)
+    file_storage.upload_file(local_profiles_filepath, gcs_profiles_filepath)
 
     #
     # SUMMARIZE BY COMMUNITY...
     #
 
-    for community_id, filtered_df in df.groupby(["community_id"]):
+    for community_id, filtered_df in profiles_df.groupby(["community_id"]):
         print("--------------")
         print("COMMUNITY", community_id, "-", len(filtered_df), "BOT PROFILES")
-        community_dirpath = os.path.join(local_dirpath, f"community-{community_id}")
-        top_tokens_filepath = os.path.join(community_dirpath, "top_profile_tokens.csv")
-        top_tags_filepath = os.path.join(community_dirpath, "top_profile_tags.csv")
+        local_community_dirpath = os.path.join(file_storage.local_dirpath, f"community-{community_id}")
+        gcs_community_dirpath = os.path.join(file_storage.gcs_dirpath, f"community-{community_id}")
+        os.makedirs(local_community_dirpath)
 
-        top_tokens_df = summarize_token_frequencies(filtered_df["profile_tokens"].tolist())
-        print(top_tokens_df.head())
-        top_tokens_df.to_csv(top_tokens_filepath)
+        tokens_df = summarize_token_frequencies(filtered_df["profile_tokens"].tolist())
+        print(tokens_df.head())
+        local_tokens_filepath = os.path.join(local_community_dirpath, "profile_tokens.csv")
+        gcs_tokens_filepath = os.path.join(gcs_community_dirpath, "profile_tokens.csv")
+        tokens_df.to_csv(local_tokens_filepath)
+        file_storage.upload_file(local_tokens_filepath, gcs_tokens_filepath)
 
-        top_tags_df = summarize_token_frequencies(filtered_df["profile_tags"].tolist())
-        print(top_tags_df.head())
-        top_tags_df.to_csv(top_tags_filepath)
+        tags_df = summarize_token_frequencies(filtered_df["profile_tags"].tolist())
+        print(tags_df.head())
+        local_tags_filepath = os.path.join(local_community_dirpath, "profile_tags.csv")
+        gcs_tags_filepath = os.path.join(gcs_community_dirpath, "profile_tags.csv")
+        tags_df.to_csv(local_tags_filepath)
+        file_storage.upload_file(local_tags_filepath, gcs_tags_filepath)
