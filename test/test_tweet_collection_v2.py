f5a2d1f7c9065814fb4e664a59224afd62ef8eef
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-16 16:55:29 -0400

    Current strategy

---
+++ test/test_tweet_collection_v2.py
@@ -0,0 +1,15 @@
+
+
+
+
+from app.tweet_collection_v2.stream_listener import TweetCollector
+
+def test_backoff_strategy():
+
+    collector = TweetCollector()
+
+    assert collector.backoff_strategy(1) == 4
+    assert collector.backoff_strategy(10) == 121
+    assert collector.backoff_strategy(100) == 10201
+    assert collector.backoff_strategy(1000) == 1002001
+    assert collector.backoff_strategy(10000) == 100020001
