eb8dd8792e121690a0b907f073b62c752865d021
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-28 16:19:12 -0400

    Tokenize hashtags

--- app/bot_analysis/top_status_tags.py
+++ app/bot_analysis/top_status_tags.py
@@ -2,6 +2,8 @@
 import os
 
 from pandas import DataFrame, read_csv
+import re
+
 from app.bq_service import BigQueryService
 from app.file_storage import FileStorage
 from app.job import Job
@@ -10,6 +12,8 @@ from app.decorators.number_decorators import fmt_n
 LIMIT = os.getenv("LIMIT") # 1000 # None  # os.getenv("LIMIT")
 BATCH_SIZE = int(os.getenv("BATCH_SIZE", default="25_000")) # 100
 DESTRUCTIVE = (os.getenv("DESTRUCTIVE", default="false") == "true") # True
+
+TWITTER_PATTERN = r'[^a-zA-Z ^0-9 # @]' # alphanumeric, plus hashtag and handle symbols (twitter-specific)
 
 def download_tweets():
     job = Job()
@@ -28,18 +32,32 @@ def download_tweets():
 
     return DataFrame(records)
 
+def parse_hashtags(status_text):
+    txt = re.sub(TWITTER_PATTERN, "", status_text.upper())
+    tags = [token.upper() for token in status_text.split() if token.startswith("#") and not token.endswith("#")]
+    return tags
+
 if __name__ == "__main__":
 
     storage = FileStorage(dirpath="bot_analysis")
-    csv_filepath = os.path.join(storage.local_dirpath, "tag_tweets.csv")
 
-    if os.path.isfile(csv_filepath) and not DESTRUCTIVE:
+    tweets_csv_filepath = os.path.join(storage.local_dirpath, "tag_tweets.csv")
+    if os.path.isfile(tweets_csv_filepath) and not DESTRUCTIVE:
         print("LOADING TWEETS...")
-        df = read_csv(csv_filepath)
+        tweets_df = read_csv(tweets_csv_filepath)
     else:
         print("DOWNLOADING TWEETS...")
-        df = download_tweets()
-        df.to_csv(csv_filepath, index=False)
+        tweets_df = download_tweets()
+        tweets_df.to_csv(tweets_csv_filepath, index=False)
+    print(fmt_n(len(tweets_df)))
+    print(tweets_df.head())
 
-    print(fmt_n(len(df)))
-    print(df.head())
+    print("ANALYZING TWEETS...")
+    tags_csv_filepath = os.path.join(storage.local_dirpath, "top_status_tags.csv")
+    #tags_df = analyze_tweets(tweets_df)
+    #tags_df.to_csv(tags_csv_filepath, index=False)
+
+    tweets_df["status_tags"] = tweets_df["status_text"].apply(parse_hashtags)
+
+    breakpoint()
+    tweets_df.to_csv(tags_csv_filepath)
