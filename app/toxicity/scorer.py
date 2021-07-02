5e35f7c50e7126bbd56d4be913620cfbae83f96a
MJ Rossetti <s2t2@users.noreply.github.com>
2021-07-01 20:18:40 -0400

    Organizing

--- app/toxicity/scorer.py
+++ app/toxicity/scorer.py
@@ -2,6 +2,9 @@
 import os
 from functools import lru_cache
 #from pprint import pprint
+from threading import current_thread, BoundedSemaphore
+from concurrent.futures import ThreadPoolExecutor, as_completed # see: https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
+
 
 from dotenv import load_dotenv
 from detoxify import Detoxify
@@ -14,8 +17,10 @@ from app.bq_service import BigQueryService, generate_timestamp, split_into_batch
 load_dotenv()
 
 MODEL_NAME = os.getenv("MODEL_NAME", default="original") # "original" or "unbiased" (see README)
-LIMIT = os.getenv("LIMIT", default="25_000") # number of records to fetch from bq at a time
+LIMIT = int(os.getenv("LIMIT", default="25_000")) # number of records to fetch from bq at a time
 BATCH_SIZE = int(os.getenv("BATCH_SIZE", default="1_000")) # number of texts to score at a time (ideal is 1K, see README)
+
+MAX_THREADS = int(os.getenv("MAX_THREADS", default=10)) # the max number of threads to use, for concurrent processing
 
 class ToxicityScorer:
     def __init__(self, model_name=MODEL_NAME, limit=LIMIT, batch_size=BATCH_SIZE, bq_service=None):
@@ -27,17 +32,21 @@ class ToxicityScorer:
         print("----------------")
         print("TOXICITY SCORER...")
         print("  MODEL:", self.model_name.upper())
-        print("  LIMIT:", self.limit)
-        print("  BATCH SIZE:", self.batch_size)
+        print("  LIMIT:", fmt_n(self.limit))
+        print("  BATCH SIZE:", fmt_n(self.batch_size))
 
     @property
     @lru_cache(maxsize=None)
-    def model(self):
-        return Detoxify(self.model_name)
-
-    @property
     def scores_table_name(self):
         return f"{self.bq_service.dataset_address}.toxicity_scores_{self.model_name}"
+
+    def count_scores(self):
+        sql = f"""
+            SELECT count(DISTINCT status_text_id) as text_count
+            FROM `{self.scores_table_name}`
+        """
+        results = self.bq_service.execute_query(sql) # API call
+        return list(results)[0]["text_count"]
 
     def fetch_texts(self):
         sql = f"""
@@ -54,52 +63,62 @@ class ToxicityScorer:
 
     @property
     @lru_cache(maxsize=None)
+    def model(self):
+        return Detoxify(self.model_name)
+
+    def process_batch(self, batch):
+        scores = self.model.predict([row["status_text"] for row in batch])
+        scores["status_text_id"] = [row["status_text_id"] for row in batch]
+
+        scores_df = DataFrame(scores)
+        # reorder columns for BQ (so they save properly):
+        scores_df = scores_df.reindex(self.scores_table_colnames, axis="columns")
+        # round scores, to reduce storage requirements:
+        for scores_col in self.model.class_names:
+            scores_df[scores_col] = scores_df[scores_col].round(8)
+        #self.save_score_records(scores_df.to_dict("records"))
+        self.save_scores(scores_df.to_dict(orient="split")["data"])
+
+    @property
+    @lru_cache(maxsize=None)
+    def scores_table_colnames(self):
+        """Returns the column names in the proper order."""
+        return ["status_text_id"] + self.model.class_names
+
+    #def save_score_records(self, records):
+    #    self.bq_service.insert_records_in_batches(self.scores_table, records) # API call
+
+    def save_scores(self, values):
+        """Params : values (list of lists corresponding with the proper column order)"""
+        return self.bq_service.client.insert_rows(self.scores_table, values) # API call
+
+    @property
+    @lru_cache(maxsize=None)
     def scores_table(self):
         return self.bq_service.client.get_table(self.scores_table_name) # API call
 
-    def save_scores(self, records):
-        self.bq_service.insert_records_in_batches(self.scores_table, records) # API call
 
-    def count_scores(self):
-        sql = f"""
-            SELECT count(DISTINCT status_text_id) as text_count
-            FROM `{self.scores_table_name}`
-        """
-        results = self.bq_service.execute_query(sql) # API call
-        return list(results)[0]["text_count"]
+
+
 
     def perform(self):
         print("----------------")
         print(f"FETCHING TEXTS...")
-        texts = list(self.fetch_texts())
+        rows = list(self.fetch_texts())
 
         print(f"ASSEMBLING BATCHES...")
-        batches = list(split_into_batches(texts, batch_size=self.batch_size))
+        batches = list(split_into_batches(rows, batch_size=self.batch_size))
 
         print(f"SCORING TEXTS IN BATCHES...")
+        counter = 0
         for index, batch in enumerate(batches):
-            print(f"  BATCH {index+1} OF {len(batches)} | {len(batch)} ITEMS |", generate_timestamp())
-
-            status_text_ids = [row["status_text_id"] for row in batch]
-            status_texts = [row["status_text"] for row in batch]
-
-            scores = self.model.predict(status_texts)
-            scores["status_text_id"] = status_text_ids
-
-            scores_df = DataFrame(scores)
-            # reorder columns for BQ (or else they won't save properly):
-            scores_table_cols = ["status_text_id"] + self.model.class_names
-            scores_df = scores_df.reindex(scores_table_cols, axis="columns")
-            # round scores, to reduce storage requirements:
-            for scores_col in self.model.class_names:
-                scores_df[scores_col] = scores_df[scores_col].round(8)
-
-            records = scores_df.to_dict("records")
-            self.save_scores(records)
+            counter += len(batch)
+            print("  ", generate_timestamp(), f"BATCH {index+1}", f"| {fmt_n(counter)}")
+            self.process_batch(batch)
 
     def perform_better(self):
         print("----------------")
-        print(f"FETCHING AND SCORING TEXTS IN BATCHES...")
+        print(f"FETCHING TEXTS. SCORING IN BATCHES...")
 
         batch = []
         batch_counter=0
@@ -110,25 +129,23 @@ class ToxicityScorer:
 
             if len(batch) >= self.batch_size:
                 batch_counter +=1
-                print(f"  BATCH {batch_counter} | {len(batch)} ITEMS |", generate_timestamp())
-
-                status_text_ids = [row["status_text_id"] for row in batch]
-                status_texts = [row["status_text"] for row in batch]
-
-                scores = self.model.predict(status_texts)
-                scores["status_text_id"] = status_text_ids
-
-                scores_df = DataFrame(scores)
-                # reorder columns for BQ (or else they won't save properly):
-                scores_table_cols = ["status_text_id"] + self.model.class_names
-                scores_df = scores_df.reindex(scores_table_cols, axis="columns")
-                # round scores, to reduce storage requirements:
-                for scores_col in self.model.class_names:
-                    scores_df[scores_col] = scores_df[scores_col].round(8)
-
-                records = scores_df.to_dict("records")
-                self.save_scores(records)
+                print("  ", generate_timestamp(), f"BATCH {batch_counter}", "|", fmt_n(counter))
+                self.process_batch(batch)
                 batch = []
+
+    #def perform_async(self, max_threads=MAX_THREADS):
+    #    with ThreadPoolExecutor(max_workers=max_threads, thread_name_prefix="THREAD") as executor:
+#
+    #        futures = [
+    #            executor.submit(process_batch_of_texts, bq_service, batch_of_texts, model, scores_table)
+    #            for batch_of_texts in batches_of_texts
+    #        ]
+#
+    #        print("FUTURE RESULTS", len(futures))
+    #        for index, future in enumerate(as_completed(futures)):
+    #            future.result()
+
+
 
 
 
@@ -139,11 +156,17 @@ if __name__ == "__main__":
     print("----------------")
     print("SCORES COUNT:", fmt_n(scorer.count_scores()))
 
-    #scorer.perform()
-    scorer.perform_better()
+    scorer.perform()
+    #scorer.perform_better()
+    #scorer.perform_better_timed()
+    #duration_seconds = scorer.perform_better_timed()
+    #items_per_second = round(scorer.limit / duration_seconds, 2)
+    #print(f"PROCESSED {fmt_n(scorer.limit)} ITEMS IN {duration_seconds} SECONDS ({items_per_second} ITEMS / SECOND)")
 
     print("----------------")
     print("JOB COMPLETE!")
+
+    print("----------------")
     print("SCORES COUNT:", fmt_n(scorer.count_scores()))
 
     server_sleep(seconds=10*60) # give the server a break before restarting
